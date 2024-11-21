e45eb6a20eb9c4965c93e2c3f65aa686915b64d4
Lit-to <litto0315@gmail.com>
2024-11-21 22:27:41 +0900

    cppが動かせるようになったのでコミット

--- contest/ABC379/a.cpp
+++ contest/ABC379/a.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << 10000;
+    string S;
+    cin >> S;
+    cout << S[1] << S[2] << S[0] << " " << S[2] << S[0] << S[1] <<endl;
     return 0;
 }
