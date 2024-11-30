9a73360eb4178a0c58dc5170a7325712a9bdb33c
Lit-to <litto0315@gmail.com>
2024-11-30 14:47:03 +0900

    TLEあり

--- contest/ABC348/d.py
+++ contest/ABC348/d.py
@@ -15,8 +15,8 @@ MAX=10**18
 
 # 便利関数定義
 def input(): return (sys.stdin.readline()).rstrip()
-def yes(f=None): print("Yes") if (f==None or f) else None; exit() if f!=None else None
-def no(f=None): print("No") if (f==None or f) else None;exit() if f!=None else None
+def yes(f=None): print("Yes") if (f==None or f) else None; exit() if (f==None or f) else None
+def no(f=None): print("No") if (f==None or f) else None;exit() if (f==None or f) else None
 def printe(*values: object,sep: str | None = " ",end: str | None = "\n",): print(*values,sep=sep,end=end); exit() #Cpythonでは動かない
 def listr(l:list): return "".join(l)
 def debug(*values: object,sep: str | None = " ",end: str | None = "\n",): print(*values,sep=sep,end=end,file=sys.stderr) #デバッグ出力用
@@ -25,8 +25,24 @@ def printYN(f:bool): yes() if f else no()
 
 
 # 関数定義スペース
+class priorityQueue():#heapqラッパー
+    def __init__(self,l=list()):#インスタンス化
+        self.queue=l.copy()
+        heapq.heapify(self.queue)
+    def __getitem__(self,i):
+        return self.queue[i]
+    def __len__(self):#かぞえる
+        return len(self.queue)
+    def __str__(self):#出力用
+        return str(self.queue)
+    def enq(self,value):#入れる
+        return heapq.heappush(self.queue,value)
+    def add(self,value):#入れる
+        return heapq.heappush(self.queue,value)
+    def deq(self):#出す
+        return heapq.heappop(self.queue)
 class Board:
-    """_summary_
+    """
     ボードを使いやすくするクラス
     一次元配列で仮想的に二次元配列を定義する
     初期化時にコンストラクタでサイズHWと初期値を指定し、読み書きが可能
@@ -43,7 +59,8 @@ class Board:
         default: int | str | bool,
         can_loop: bool = True,
     ) -> None:
-        """初期化関数 インスタンス生成時に呼び出される
+        """
+        初期化関数 インスタンス生成時に呼び出される
 
         Args:
             h (int): 縦の長さ
@@ -81,7 +98,7 @@ class Board:
         grid = []
         w = self.get_width()
         h = self.get_height()
-        for i in range(0, h - 1):
+        for i in range(0, h):
             grid.append(self.__board[i * w : w * (i + 1)])
         return grid
 
@@ -181,12 +198,70 @@ class Board:
 H, W = map(int,input().split())
 BOARD=Board(H+1,W+1,lambda x:list(input())+["#"] if x<H else ["#"]*(W+1),can_loop=True)
 N = int(input())
+MAP=dict(lambda:0)
 for i in range(N):
     r,c,e=map(int,input().split())
-    BOARD.set_value(r-1,c-1,e)
+    pos=BOARD.get_index(r-1,c-1)
+    MAP[pos]=e
 # QUERY=[tuple(map(int,input().split())) for i in range(N)]
 
+health_board=Board(H+1,W+1,lambda x:[0]*(W+1),can_loop=True)
 
 # 処理スペース ================================================================================================Lit_to
-BOARD.print(debug)
-print(BOARD)
+#デバッグ用
+# DEBUGBOARD=Board(H+1,W+1,lambda x:["#"]*(W+1),can_loop=True)
+# for i in range(H):
+#     for j in range(W):
+#         DEBUGBOARD.set_value(i,j,BOARD.get_value(i,j))
+# for i in MAP:
+#     y,x=DEBUGBOARD.get_pos(i)
+#     DEBUGBOARD.set_value(y,x,MAP[i])
+
+# DEBUGBOARD.print(debug)
+#ここまで
+
+
+start=(0,0)
+goal=(0,0)
+for i in range(H):
+    for j in range(W):
+        if BOARD.get_value(i,j)=="S":
+            start=(i,j)
+
+task=priorityQueue()
+task.add(
+    [
+        MAP[BOARD.get_index(start[0],start[1])],
+        start,
+        set([BOARD.get_index(start[0],start[1])])
+    ]
+    )
+RLUD=[[0,1],[0,-1],[-1,0],[1,0]]
+
+
+while task:
+    now=task.deq()
+    for i in RLUD:
+        health,pos,visited=now[0],now[1],now[2].copy()
+        new_pos=pos[0]+i[0],pos[1]+i[1]
+        index=BOARD.get_index(new_pos[0],new_pos[1])
+        if index in visited:
+            continue
+        value=BOARD.get_value(new_pos[0],new_pos[1])
+        yes(value=="T")
+        if value=="#":
+            continue
+        elif value==".":
+            if health<=0:
+                break
+            elif health<health_board.get_value(new_pos[0],new_pos[1]):
+                continue
+            else:
+                health-=1
+                health=max(MAP[index],health)
+                health_board.set_value(new_pos[0],new_pos[1],health)
+                visited.add(index)
+                task.add([health,new_pos,visited])
+no()
+
+
