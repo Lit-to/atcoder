8ab2395cf6a5ca356361c18ebdb4b2dce8ea7ba8
Lit-to <litto0315@gmail.com>
2024-12-01 23:33:41 +0900

    C問題をACしたよ

--- contest/ABC382/c.py
+++ contest/ABC382/c.py
@@ -25,7 +25,7 @@ def printYN(f:bool): yes() if f else no()
 
 
 # 関数定義スペース
-def search(ok:int,ng:int,f:bool)->int: #二分探索原型
+def search(ok:int,ng:int,f:bool)->int:
     # okは条件を満たす領域の外側
     # ngは条件を満たさない領域の外側
     # fは条件を満たすかどうかの評価関数
@@ -39,94 +39,29 @@ def search(ok:int,ng:int,f:bool)->int: #二分探索原型
             ng=mid
     return ok
 
-def eval_p(x):
-    return A[i]<=sushi[x][0]
-
-class SegTree:
-    """
-    init(init_val, ide_ele): 配列init_valで初期化 O(N)
-    update(k, x): k番目の値をxに更新 O(logN)
-    query(l, r): 区間[l, r)をsegfuncしたものを返す O(logN)
-    """
-    def __init__(self, init_val, segfunc, ide_ele):
-        """
-        init_val: 配列の初期値
-        segfunc: 区間にしたい操作
-        ide_ele: 単位元
-        n: 要素数
-        num: n以上の最小の2のべき乗
-        tree: セグメント木(1-index)
-        """
-        n = len(init_val)
-        self.segfunc = segfunc
-        self.ide_ele = ide_ele
-        self.num = 1 << (n - 1).bit_length()
-        self.tree = [ide_ele] * 2 * self.num
-        # 配列の値を葉にセット
-        for i in range(n):
-            self.tree[self.num + i] = init_val[i]
-        # 構築していく
-        for i in range(self.num - 1, 0, -1):
-            self.tree[i] = self.segfunc(self.tree[2 * i], self.tree[2 * i + 1])
-    
-    def update(self, k, x):
-        """
-        k番目の値をxに更新
-        k: index(0-index)
-        x: update value
-        """
-        k += self.num
-        self.tree[k] = x
-        while k > 1:
-            self.tree[k >> 1] = self.segfunc(self.tree[k], self.tree[k ^ 1])
-            k >>= 1
-
-    def query(self, l, r):
-        """
-        [l, r)のsegfuncしたものを得る
-        l: index(0-index)
-        r: index(0-index)
-        """
-        res = self.ide_ele
-
-        l += self.num
-        r += self.num
-        while l < r:
-            if l & 1:
-                res = self.segfunc(res, self.tree[l])
-                l += 1
-            if r & 1:
-                res = self.segfunc(res, self.tree[r - 1])
-            l >>= 1
-            r >>= 1
-        return res
-
-def eval_s(x,y):
-    return min(x,y)
 # 入力スペース ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Lit_to
 N, M = map(int,input().split())
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
 # 処理スペース ================================================================================================Lit_to
-sushi=[]
-for i in range(M):
-    sushi.append((B[i],i))
-sushi.sort()
-people=[]
+mn=MAX
+
+A_who_can_eats=[]
+
 for i in range(N):
-    people.append(sushi[i][1])
+    if A[i]<=mn:
+        A_who_can_eats.append((A[i],i+1))
+        mn=A[i]
+    else:
+        continue
 
-seg=SegTree(people,eval_s,MAX)
+A_who_can_eats.append((0,-1))
+N=len(A_who_can_eats)
+# A_who_can_eats.reverse()
+# debug(A_who_can_eats)
 
-result=[]
 for i in range(M):
-    fg=search(0,N,A[i]<=sushi[i][0])
-    result.append(seg.query(0,fg))
+    result=search(N,-1,lambda x:A_who_can_eats[x][0]<=B[i])
+    print(A_who_can_eats[result][1])
 
-# ate=[-1]*N
-# for i in range(N):
-#     ate[i]=search(M,-1,eval_p)
-for i in range(M):
-    result.append(seg.query(0,sushi[i][]))
 
-print(*result)
