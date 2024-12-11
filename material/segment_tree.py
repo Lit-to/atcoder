effbd090a8d96dc1a1f78cdba7d186629fc6be5f
Lit-to <litto0315@gmail.com>
2024-12-11 20:52:38 +0900

    必要ないファイルの削除 テンプレフォルダに追加

--- material/segment_tree.py
+++ material/segment_tree.py
@@ -1,30 +1,3 @@
-# インポート(本編はだいたい30行目あたり)
-import sys,itertools,math,heapq,pypyjit
-from collections import defaultdict,deque
-from sortedcontainers import SortedSet, SortedList, SortedDict #Cpythonでは動かない(importにも多少時間がかかる)
-pypyjit.set_param('max_unroll_recursion=-1')
-sys.setrecursionlimit(10**8)
-sys.set_int_max_str_digits(0)
-dict=defaultdict
-
-# 便利定数定義
-ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-# ALPHABET="abcdefghijklmnopqrstuvwxyz"
-MOD=998244353
-MAX=10**18
-
-# 便利関数定義
-def input(): return (sys.stdin.readline()).rstrip()
-def yes(f=None): print("Yes") if (f==None or f) else None; exit() if f!=None else None
-def no(f=None): print("No") if (f==None or f) else None;exit() if f!=None else None
-def printe(*values: object,sep: str | None = " ",end: str | None = "\n",): print(*values,sep=sep,end=end); exit() #Cpythonでは動かない
-def listr(l:list): return "".join(l)
-def debug(*values: object,sep: str | None = " ",end: str | None = "\n",): print(*values,sep=sep,end=end,file=sys.stderr) #デバッグ出力用
-def look(pos:tuple,board:list): return board[pos[0]][pos[1]] #HWボードの(i,j)の値を参照して返す関数
-def printYN(f:bool): yes() if f else no()
-
-
-# 関数定義スペース
 class segmentTree(list):
     def __init__(self,values,default=-1,rule=lambda x,y:max(x,y)):
         """
@@ -160,25 +133,3 @@ class segmentTree(list):
         assert 0<=left , "left value needs more than 0"
         assert right<=self.value_length , "right value needs lower than max length"
         return self.__check_rule(left,right,1,self.half_size,1)
-
-# # 入力スペース ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Lit_to
-N, Q = map(int,input().split())
-query=[]
-for i in range(Q):
-    query.append(tuple(map(int,input().split())))
-A=[0]*N
-# # 処理スペース ================================================================================================Lit_to
-
-seg_tree=segmentTree(A,rule=lambda x,y:max(x,y))
-
-for i in query:
-    # debug(seg_tree.data[seg_tree.half_size:seg_tree.half_size+seg_tree.value_length])
-    if i[0]==1:
-        t,pos,x=i
-        seg_tree.update(pos,x)
-    elif i[0]==2:
-        t,l,r=i
-        if r-1==l:
-            print(seg_tree.data[seg_tree.half_size-1+l])
-            continue
-        print(seg_tree.get_query(l,r-1))
