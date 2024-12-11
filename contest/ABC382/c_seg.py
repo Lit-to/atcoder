2cba8adc8b8ac956b72c2f77ae27650c627a703b
Lit-to <litto0315@gmail.com>
2024-12-11 22:55:15 +0900

    C セグ木ルートでAC

--- contest/ABC382/c_seg.py
+++ contest/ABC382/c_seg.py
@@ -1,3 +1,45 @@
+# インポート(本編はだいたい30行目あたり)
+import sys,itertools,math,heapq,pypyjit
+from collections import defaultdict,deque
+from sortedcontainers import SortedSet, SortedList, SortedDict #Cpythonでは動かない(importにも多少時間がかかる)
+pypyjit.set_param('max_unroll_recursion=-1')
+sys.setrecursionlimit(10**8)
+sys.set_int_max_str_digits(0)
+dict=defaultdict
+
+# 便利定数定義
+ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# ALPHABET="abcdefghijklmnopqrstuvwxyz"
+MOD=998244353
+MAX=10**18
+
+# 便利関数定義
+def input(): return (sys.stdin.readline()).rstrip()
+def yes(f=None): print("Yes") if (f==None or f) else None; exit() if f!=None else None
+def no(f=None): print("No") if (f==None or f) else None;exit() if f!=None else None
+def printe(*values: object,sep: str | None = " ",end: str | None = "\n",): print(*values,sep=sep,end=end); exit() #Cpythonでは動かない
+def listr(l:list): return "".join(l)
+def debug(*values: object,sep: str | None = " ",end: str | None = "\n",): print(*values,sep=sep,end=end,file=sys.stderr) #デバッグ出力用
+def look(pos:tuple,board:list): return board[pos[0]][pos[1]] #HWボードの(i,j)の値を参照して返す関数
+def printYN(f:bool): yes() if f else no()
+
+
+# 関数定義スペース
+
+def search(ok:int,ng:int,f:bool)->int:
+    # okは条件を満たす領域の外側
+    # ngは条件を満たさない領域の外側
+    # fは条件を満たすかどうかの評価関数
+        # lambda i:a[i]<x xを含まない最大のiを返す
+        # lambda i:a[i]<=x xを含む最大のiを返す
+    while 1<abs(ok-ng):
+        mid=(ng+ok)//2
+        if f(mid):
+            ok=mid
+        else:
+            ng=mid
+    return ok
+
 class segmentTree(list):
     def __init__(self,values,default=-1,rule=lambda x,y:max(x,y)):
         """
@@ -194,4 +236,37 @@ class segmentTree(list):
         assert 0<=left , "left value needs more than 0"
         assert right<=self.__value_length , "right value needs lower than max length"
         return self.__check_rule(left,right,1,self.__center,1)
+
+
+# 入力スペース ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Lit_to
+N, M = map(int,input().split())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+# 処理スペース ================================================================================================Lit_to
+seats_appetite=[-1]*N
+for i in range(N):
+    seats_appetite[i]=(A[i],i+1)
+seats_appetite.sort(reverse=False)
+
+seats=[-1]*N
+for i in range(N):
+    seats[i]=seats_appetite[i][1]
+
+segTree=segmentTree(seats,default=MAX,rule=lambda x,y:min(x,y))
+debug(segTree)
+
+
+for i in B:
+    index=search(-1,N,lambda x:seats_appetite[x][0]<=i)
+    if index==-1:
+        print(-1)
+    elif index==0:
+        print(seats_appetite[0][1])
+    else:
+        print(segTree.get_query(1,index+1))
+
+
+
+
+
 
