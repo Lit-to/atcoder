0d04f868cfd27f50d2fdcd07cb7b44115e6cd371
Lit-to <litto0315@gmail.com>
2024-12-11 22:11:42 +0900

    セグ木完成

--- material/segment_tree.py
+++ material/segment_tree.py
@@ -2,7 +2,7 @@ class segmentTree(list):
     def __init__(self,values,default=-1,rule=lambda x,y:max(x,y)):
         """
         初期化関数 これらのデータをもとにセグメント木を構築する。
-        self.rule(x,y)は、x,tのうち優先度が大きいほうの値が返却される関数としてユーザが定義する
+        self.__rule(x,y)は、x,tのうち優先度が大きいほうの値が返却される関数としてユーザが定義する
         Args:
             values (list) :初期化対象の配列
             default (int) :初期化設定がなかったときの初期値(更新時に一番更新の優先度が低くなる値) デフォルトは-1
@@ -11,22 +11,22 @@ class segmentTree(list):
             ValueError:valuesに空配列が指定された場合
         """
         # super().__init__(values)
-        self.value_length=len(values)
-        self.half_size=self.__calc_size()      
-        self.size=self.half_size*2
-        self.data=[default]*(self.size+2) #1-indexedのための端を追加
-        self.default=default
-        self.rule=rule
-
-        for i in range(self.value_length):#値の上書き
-            self.data[i+self.half_size]=values[i]
+        self.__value_length=len(values) # 実際の値の長さ
+        self.__center=self.__calc_size() # 完全二分木として表す配列の長さの半分(値が格納できる大きさのうち一番小さい値をiとしたときの2^iで、実際の値が入る部分の長さ)
+        self.__size=self.__center*2 # MSbの2倍の長さの配列
+        self.__data=[default]*(self.__size+2) #1-indexedのための端を追加
+        self.__default=default # 初期値
+        self.__rule=rule # 値更新ルール
+        
+        for i in range(self.__value_length):#値の上書き
+            self.__data[i+self.__center]=values[i]
         
         self.__struct(2)
 
     def __struct(self,index:int):
         """
         セグメント木を再帰的に構築する関数
-        self.ruleをここで利用する
+        self.__ruleをここで利用する
 
         Args:
             index (int): 値を代入するインデックス
@@ -34,14 +34,14 @@ class segmentTree(list):
         Returns:
             _type_: 代入された値を返す
         """
-        value=self.default
-        if self.size<(index<<1):
-            return self.data[index-1]
+        value=self.__default
+        if self.__size<(index<<1):
+            return self.__data[index-1]
         else:
             right_value=self.__struct(index*2)
             left_value=self.__struct(index*2-1)
-            value=self.rule(left_value,right_value)
-            self.data[index-1]=value
+            value=self.__rule(left_value,right_value)
+            self.__data[index-1]=value
         return value
 
     def __calc_size(self):
@@ -54,7 +54,7 @@ class segmentTree(list):
             lengthのMSb(1になっているビットのうち、一番左のもの)を求める。
             1をMSb左にビットシフトする。
         """
-        length=self.value_length
+        length=self.__value_length
         if length <=1:
             return 2
         return 1<<self.__calc_msb(length)
@@ -87,21 +87,82 @@ class segmentTree(list):
             _type_: 結果
         """
         if b<l or r<a: # 対象外範囲の場合はルール最弱の値を返す
-            return self.default
+            return self.__default
         elif a<=l and r<=b:#この時点で答えが確定している場合は答えを返す
-            return self.data[k]
+            return self.__data[k]
         elif r==l: # lrがぶつかってしまった場合は区間内にいるかどうかをチェック
             if a<=r and r<=b: #区間内の場合は今見える値を返す
-                return self.data[k]
+                return self.__data[k]
             else: #区間外の場合はルール最弱の値を返す
-                return self.default
+                return self.__default
         
         # 以下、範囲が調べたい範囲の境界線を踏んでいる場合
         # 今の区間では判断できないので細分して再起呼び出しで判断する
         left=self.__check_rule(a,b,l,l+(r-l)//2,2*k) 
         right=self.__check_rule(a,b,l+1+(r-l)//2,r,2*k+1)
-        return self.rule(left,right)
+        return self.__rule(left,right)
+
+    ### 組み込み系対応関数
+
+    def __len__(self):
+        """
+        len()が呼び出されたときの関数
+        __value_length、つまり初期化時の配列長を返す
+        Returns:
+            int: 配列長
+        """
+        return self.__value_length
     
+    def __str__(self):
+        """
+        str()が呼び出されたときの関数
+        配列をstr()したものを返す
+
+        Returns:
+            str: 配列をstr()したもの
+        """
+        return str(self.__data[self.__value_length:self.__center+self.__value_length])
+
+    def __getitem__(self,item:slice|int):
+        """
+        配列にスライスやインデックスの読み出しをする関数
+        list[x]のように指定したときに呼び出される
+        初期化時の配列(list型)と同様の挙動をする
+
+        Args:
+            item (slice | int): list[x]のxにあたるインデックス(1-indexed)
+
+        Returns:
+            int: 読みだしたデータの内容
+        """
+        if type(item)==int:
+            if 0<=item:
+                return self.__data[self.__value_length+item]
+            else:
+                return self.__data[self.__value_length+self.__center+item]
+        else:
+            start,stop,step=item.start,item.stop,item.step
+            if start==None:
+                start=1
+            if stop==None:
+                stop=self.__value_length
+            if step==None:
+                step=1
+            return self.__data[self.__center+start-1:self.__center+stop-1:step]
+
+    def __setitem__(self,key:int,value:int):
+        """
+        値の更新を行う
+
+        Args:
+            key (int): インデックス(1-indexed)
+            value (int):更新する値 
+        """
+        self.update(key,value)
+
+
+    ###セグメント木らしい関数↓
+
     def update(self,index:int,value:int):
         """
         配列のindex番目にvalueを代入し木の更新を行う。
@@ -110,20 +171,20 @@ class segmentTree(list):
             index (int): 代入したい位置を示すインデックス
             value (int): 代入したい値
         """
-        assert 0<=index<=self.value_length,"illigal index("+str(index)+")"
-        index+=self.half_size-1
-        self.data[index]=value
+        assert 0<=index<=self.__value_length,"illigal index("+str(index)+")"
+        index+=self.__center-1
+        self.__data[index]=value
         NEIGHBOR=[-1,1]
         while 2<=index:
             next=index>>1
             diff=NEIGHBOR[index%2==0]
-            new_value=self.rule(self.data[index],self.data[index+diff])
-            self.data[next]=new_value
+            new_value=self.__rule(self.__data[index],self.__data[index+diff])
+            self.__data[next]=new_value
             index=next
 
     def get_query(self,left:int,right:int):
         """
-        (left:right)のうち、self.ruleの優先順位が一番高い値を返す
+        (left:right)のうち、self.__ruleの優先順位が一番高い値を返す
 
         Args:
             left (int): 区間のインデックスの左端
@@ -131,5 +192,6 @@ class segmentTree(list):
         """
         assert left<right , "left value needs lower than right"
         assert 0<=left , "left value needs more than 0"
-        assert right<=self.value_length , "right value needs lower than max length"
-        return self.__check_rule(left,right,1,self.half_size,1)
+        assert right<=self.__value_length , "right value needs lower than max length"
+        return self.__check_rule(left,right,1,self.__center,1)
+
