bb192ffdb4837da9275cc4566fdbebca01d95325
Lit-to <litto0315@gmail.com>
2024-12-22 16:12:46 +0900

    折角なのでP C 階乗の関数を整備

--- contest/ABC137/c.py
+++ contest/ABC137/c.py
@@ -25,16 +25,51 @@ def printYN(f:bool): yes() if f else no()
 
 
 # 関数定義スペース
-class litMath:
-    def factorial(n:int):
-        return litMath.permutation(n,n)
-    def permutation(n:int,k:int):
-        result=1
-        for i in range(n,n-k,-1):
-            result*=i
+class lit_math:
+    def factorial(n: int) -> int:
+        """
+        n!の結果を返す。
+
+        より具体的には整数nの階乗の値を返す。
+
+        Args:
+            n(int):nの値
+        
+        Returns:
+            int: 計算結果
+        """
+        return lit_math.permutation(n, n)
+
+    def permutation(n: int, k: int) -> int:
+        """
+        nPkの値を返す。
+        より具体的にはn個の整数からk個を順番に並べられる通り数を返す。
+
+        Args:
+            n(int):nの値
+            k(int):kの値
+        
+        Returns:
+            int: 計算結果
+        """
+        result = 1
+        for i in range(n, n - k, -1):
+            result *= i
         return result
-    def combination(n:int,k:int):
-        return litMath.permutation(n,k)//litMath.factorial(k)
+
+    def combination(n: int, k: int) -> int:
+        """
+        nCkの値を返す。
+        より具体的にはn個の整数からk個を選ぶ組み合わせ数を返す。
+
+        Args:
+            n(int):nの値
+            k(int):kの値
+
+        Returns:
+            int: 計算結果
+        """
+        return lit_math.permutation(n, k) // lit_math.factorial(k)
 
 
 # 入力スペース ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Lit_to
@@ -47,7 +82,7 @@ for i in S:
 result=0
 for i in d:
     if 1<d[i]:
-        result+=(litMath.combination(d[i],2))
+        result+=(lit_math.combination(d[i],2))
 print(result)
 
 
