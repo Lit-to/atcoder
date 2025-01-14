2105ea31c20bb54b5886ae33edd51638300abc3b
Lit-to <litto0315@gmail.com>
2025-01-15 01:05:16 +0900

    TLE

---
+++ contest/py/ABC238/c.py
@@ -0,0 +1,43 @@
+# インポート(本編はだいたい30行目あたり)
+import sys,itertools,math,heapq,pypyjit
+from collections import defaultdict,deque
+from sortedcontainers import SortedSet, SortedList, SortedDict #Cpythonでは動かない(importにも多少時間がかかる)
+pypyjit.set_param('max_unroll_recursion=-1')
+sys.setrecursionlimit(10**8)
+sys.set_int_max_str_digits(0)
+dict=defaultdict
+
+# 便利定数定義
+ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# ALPHABET="abcdefghijklmnopqrstuvwxyz"
+MOD=998244353
+MAX=10**18
+LRUD=[(0,1),(0,-1),(1,0),(-1,0)]
+LURULDRD=[(-1,-1),(-1,1),(1,-1),(1,1)]
+
+# 便利関数定義
+def input(): return (sys.stdin.readline()).rstrip()
+def printe(*values,sep=" ",end="\n"):print(*values,sep=sep,end=end);exit()
+def yes(f=True): printe("Yes") if (f) else None
+def no(f=True): printe("No") if (f) else None
+def listr(l:list,f=str): return "".join(list(map(f,l)))
+def debug(*values,sep=" ",end="\n"): print(*values,sep=sep,end=end,file=sys.stderr) #デバッグ出力用
+def printYN(f:bool): yes() if f else no()
+
+
+# 関数定義スペース
+def f(x):
+    result=0
+    target=len(str(x))
+    for i in range(1,x+1):
+        result+=1 if len(str(i))==target else 0
+    return result
+
+# 入力スペース ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Lit_to
+N = int(input())
+# 処理スペース ================================================================================================Lit_to
+result=0
+for i in range(1,N+1):
+    result+=f(i)
+
+print(result%MOD)
