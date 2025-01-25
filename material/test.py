4b44ee019e64c62be63d8dc58d9235e5cc4dfa18
Lit-to <litto0315@gmail.com>
2025-01-26 01:58:00 +0900

    fractionにdocstringと特殊メソッドを追加

--- material/test.py
+++ material/test.py
@@ -4,5 +4,55 @@
 #if __name__=="__main__":
 #    assert gcd(36,24)==12
 #    assert gcd(24,36)==12
+#
+
+# fractionテスト
+# 2025-01-26 01:57:06
+#
+#if __name__ =="__main__":
+#    frac_A=fraction(4,10)
+#    assert frac_A.get_denominator()==5
+#    assert frac_A.get_numerator()==2
+#    assert frac_A.get_value()==0.4
+#    assert str(frac_A)=="2/5"
+#
+#    frac_B=fraction(8,48)
+#    assert frac_A+frac_B==fraction(17,30)
+#    assert frac_A-frac_B==fraction(7,30)
+#    assert frac_B-frac_A==fraction(-7,30)
+#    assert frac_B*frac_A==fraction(1,15)
+#    assert frac_B//frac_A==fraction(5,12)
+#    assert frac_B/frac_A==fraction(5,12)
+#    assert (frac_A!=frac_B)==True
+#    assert (frac_A<frac_B)==False
+#    assert (frac_B<frac_A)==True
+#    assert (frac_A>frac_B)==True
+#    assert (frac_B>frac_A)==False
+#
+#    frac_C=fraction(2,5)
+#    assert (frac_A==frac_C)==True
+#    assert (frac_A<=frac_C)==True
+#    assert (frac_A>=frac_C)==True
+#    assert (frac_A<frac_C)==False
+#    assert (frac_B<frac_C)==True
+#    assert (frac_A>frac_C)==False
+#    assert (frac_B>frac_C)==False
+#
+#    frac_D=fraction(1,2)
+#    frac_D+=frac_A
+#    assert frac_D==fraction(9,10)
+#    
+#    frac_D=fraction(1,2)
+#    frac_D-=frac_A
+#    assert frac_D==fraction(1,10)
+#    
+#    frac_D=fraction(1,2)
+#    frac_D/=frac_A
+#    assert frac_D==fraction(5,4)
+#    
+#    frac_D=fraction(1,2)
+#    frac_D//=frac_A
+#    assert frac_D==fraction(5,4)
+#    
 
     
