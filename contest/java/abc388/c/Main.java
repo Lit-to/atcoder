6b8030d6aa347db39040b3980706795de271e227
Lit-to <litto0315@gmail.com>
2025-01-27 22:05:47 +0900

    C 途中

--- contest/java/abc388/c/Main.java
+++ contest/java/abc388/c/Main.java
@@ -1,40 +1,28 @@
-import java.io.*;
 import java.util.*;
+// import java.util.function.Function;
 
-public class Main {
-    //ABC388Cおもち
+// import java.util.Scanner;
+
+public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = Integer.parseInt(sc.nextLine().split(" ")[0]);
-        String[] A = sc.nextLine().split(" ");
-        int result = 0;
-        for (int i = 0; i < N; ++i) {
-            int ans = search(0, N, A, Integer.parseInt(A[i]) * 2)+1;
-            if (ans == N - 1 && Integer.parseInt(A[N-1])<=Integer.parseInt(A[i])*2) {
-                break;
-            }
-            result += N - ans;
-        }
-        System.out.println(result);
-        sc.close();
+        int N = Integer.parseInt(sc.nextLine());
+        String[] A = sc.nextLine().split(" "); 
     }
-
-    public static int search(int ok, int ng, String[] array, int value) {
-        while (1 < Math.abs(ok - ng)) {
-            int center = ((ok + ng) / 2);
-            if (check(center, array, value)) {
-                ok = center;
-            } else {
-                ng = center;
+    public static int search(int ok,int ng){
+        while (1<Math.abs(ok-ng)){
+            int mid = (ok+ng)/2;
+            if (check(mid)){
+                ng=mid;
+            }
+            else{
+                ok=mid;
             }
         }
         return ok;
     }
+    public static boolean check(int mid){
+        return true;
 
-    public static boolean check(int target, String[] array, int value) {
-        return Integer.parseInt(array[target])<value;
     }
-
 }
-// 10
-// 1 3 5 7 20 32 45 78 82 93
