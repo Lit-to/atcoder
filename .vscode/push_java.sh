27c02fcf649032d257336de138c69ee33dd04457
Lit-to <litto0315@gmail.com>
2025-02-01 16:21:22 +0900

    ファイル名に小文字を許容 フォルダ名に大文字を指定

--- .vscode/push_java.sh
+++ .vscode/push_java.sh
@@ -4,8 +4,8 @@
 SOURCE_FILE="$1"
 
 # コピー先のディレクトリパス
-FOLDER_NAME=$(head -n 1 "$SOURCE_FILE" | awk '{print $2}' | tr '[:upper:]' '[:lower:]')
-if ! [[ "$FOLDER_NAME" =~ ^abc[0-9]{3}$ ]]; then
+FOLDER_NAME=$(head -n 1 "$SOURCE_FILE" | awk '{print $2}' | tr '[:lower:]' '[:upper:]')
+if ! [[ "$FOLDER_NAME" =~ ^A[B|R|G]C[0-9]{3}$ ]]; then
     echo "Error: The folder name must be in the format 'abc' followed by three digits."
     exit 1
 fi
@@ -13,12 +13,12 @@ DEST_DIR="contest/java/$FOLDER_NAME"
 
 # コピー先のファイル名
 CLASS_NAME=$(head -n 1 "$SOURCE_FILE" | awk '{print $3}')
-if ! [[ "$CLASS_NAME" =~ ^[a-zA-Z]$ ]]; then
-    echo "Error: The class name must be a single letter."
-    exit 1
-else
-    CLASS_NAME=$(echo "$CLASS_NAME" | tr '[:upper:]' '[:lower:]')
-fi
+# if ! [[ "$CLASS_NAME" =~ ^[a-zA-Z]$ ]]; then
+#     echo "Error: The class name must be a single letter."
+#     exit 1
+# else
+#     CLASS_NAME=$(echo "$CLASS_NAME" | tr '[:upper:]' '[:lower:]')
+# fi
 DEST_FILE="${CLASS_NAME}.java"
 
 # コピー先ディレクトリが存在しない場合は作成
