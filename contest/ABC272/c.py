11db37a780674b291ca9c198b37e1b1bf26af01f
Lit-to <litto0315@gmail.com>
2025-02-04 01:08:39 +0900

    ABC272 C PyAC

---
+++ contest/ABC272/c.py
@@ -0,0 +1,67 @@
+# インポート(本編はだいたい30行目あたり)
+import sys,itertools,math,heapq,pypyjit
+from collections import defaultdict,deque
+from sortedcontainers import SortedSet, SortedList, SortedDict #Cpythonでは動かない(importにも多少時間がかかる)
+# pypyjit.set_param('max_unroll_recursion=-1') # ワンチャンはやくなるかも
+sys.setrecursionlimit(10**8)
+sys.set_int_max_str_digits(0)
+dict=defaultdict
+
+# 便利定数定義
+ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# ALPHABET="abcdefghijklmnopqrstuvwxyz"
+MOD=998244353
+MAX=10**18
+LRUD=[(0,1),(0,-1),(1,0),(-1,0)]
+LURULDRD=[(-1,-1),(-1,1),(1,-1),(1,1)]
+
+# 便利関数定義
+def input(): return (sys.stdin.readline()).rstrip()
+def printe(*values,sep=" ",end="\n"):print(*values,sep=sep,end=end);exit()
+def yes(f=True): printe("Yes") if (f) else None
+def no(f=True): printe("No") if (f) else None
+def listr(l:list,f=str): return "".join(list(map(f,l)))
+def debug(*values,sep=" ",end="\n"): print(*values,sep=sep,end=end,file=sys.stderr) #デバッグ出力用
+def printYN(f:bool): yes() if f else no()
+
+
+# 関数定義スペース
+
+# 入力スペース ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Lit_to
+N = int(input())
+# H, W = map(int,input().split())
+A = list(map(int,input().split()))
+# S = input()
+# S = list(input())
+# 処理スペース ================================================================================================Lit_to
+
+odd=[]
+even=[]
+
+for i in A:
+    if i%2==0:
+        even.append(i)
+    else:
+        odd.append(i)
+
+even.sort()
+odd.sort()
+
+cand=[]
+if 2<=len(even):
+    temp=0
+    temp+=even.pop()
+    temp+=even.pop()
+    cand.append(temp)
+
+if 2<=len(odd):
+    temp=0
+    temp+=odd.pop()
+    temp+=odd.pop()
+    cand.append(temp)
+
+if cand==[]:
+    printe(-1)
+
+print(max(cand))
+
