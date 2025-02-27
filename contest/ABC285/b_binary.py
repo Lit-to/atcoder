38d48f80f4e782ce874c29adc426a6c1e50c55af
Lit-to <litto0315@gmail.com>
2025-02-27 20:53:57 +0900

    二探解 AC

---
+++ contest/ABC285/b_binary.py
@@ -0,0 +1,67 @@
+# インポート(本編はだいたい30行目あたり)
+import sys,itertools,math,heapq
+from collections import defaultdict,deque
+from sortedcontainers import SortedSet, SortedList, SortedDict # CPython?
+# pypyjit.set_param('max_unroll_recursion=-1')
+sys.setrecursionlimit(10**8)
+sys.set_int_max_str_digits(0)
+dict=defaultdict
+
+# 便利定数定義
+ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# ALPHABET="abcdefghijklmnopqrstuvwxyz"
+MOD=998244353
+MAX=10**18
+LRUD=[(0,1),(0,-1),(1,0),(-1,0)]
+LURULDRD=[(-1,-1),(-1,1),(1,-1),(1,1)]
+
+# 便利関数定義
+def input(): return (sys.stdin.readline()).rstrip()
+def printe(*values,sep=" ",end="\n"):print(*values,sep=sep,end=end);exit()
+def yes(f=True): printe("Yes") if (f) else None
+def no(f=True): printe("No") if (f) else None
+def listr(l:list,f=str): return "".join(list(map(f,l)))
+def debug(*values,sep=" ",end="\n"): print(*values,sep=sep,end=end,file=sys.stderr)
+def look(pos:tuple,board:list): return board[pos[0]][pos[1]]
+def printYN(f:bool): yes() if f else no()
+
+
+# 関数定義スペース
+def search(ok:int,ng:int,f:bool)->int: #二分探索原型
+    # okは条件を満たす領域の外側
+    # ngは条件を満たさない領域の外側
+    # fは条件を満たすかどうかの評価関数
+        # lambda i:a[i]<x xを含まない最大のiを返す
+        # lambda i:a[i]<=x xを含む最大のiを返す
+    while 1<abs(ok-ng):
+        mid=(ng+ok)//2
+        if f(mid):
+            ok=mid
+        else:
+            ng=mid
+    return ok
+
+def check(x,S,i):
+    for j in range(x):
+        if S[j]==S[j+i]:
+            return False
+    else:
+        return True
+
+
+def main():
+    # 入力スペース ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Lit_to
+    N = int(input())
+    S = list(input())
+    # 処理スペース ================================================================================================Lit_to
+    result=[]
+    for i in range(1,N):
+        result.append(search(0,N-i+1,lambda x:check(x,S,i)))
+
+    print(*result)
+
+
+
+
+if __name__=="__main__":
+    main()
