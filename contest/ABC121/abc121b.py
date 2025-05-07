a754d17613766cf62b645958cdf5a3f5f402567a
Lit-to <litto0315@gmail.com>
2025-05-08 04:03:15 +0900

    カス

---
+++ contest/ABC121/abc121b.py
@@ -0,0 +1,76 @@
+# ABC121B
+# 関数定義スペース
+def check(code:list):
+    result=0
+    for i in range(M):
+        result+=code[i]*B[i]
+    result+=C
+    return 0<result
+
+
+
+def main():
+    # 入力スペース ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Lit_to
+    global N,M,C,B,CODES
+    N, M, C = map(int,input().split())
+    B = list(map(int,input().split()))
+    CODES=[]
+    for i in range(N):
+        CODES.append(tuple(map(int,input().split())))
+    # 処理スペース ================================================================================================Lit_to
+    result=0
+    for i in CODES:
+        result+=check(i)
+    printe(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 以下コピペ部分=============================================================
+# インポート(本編はだいたい30行目あたり)
+import sys,itertools,math,heapq
+from collections import defaultdict,deque
+from sortedcontainers import SortedSet, SortedList, SortedDict # CPython?
+# pypyjit.set_param('max_unroll_recursion=-1')
+sys.setrecursionlimit(10**8)
+sys.set_int_max_str_digits(0)
+dict=defaultdict
+# 便利定数定義
+ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# ALPHABET="abcdefghijklmnopqrstuvwxyz"
+MOD=998244353
+MAX=10**18
+LRUD=[(0,1),(0,-1),(1,0),(-1,0)]
+LURULDRD=[(-1,-1),(-1,1),(1,-1),(1,1)]
+# 便利関数定義
+def input(): return (sys.stdin.readline()).rstrip()
+def inputs(): return map(int,input().split())
+def printe(*values,sep=" ",end="\n"):print(*values,sep=sep,end=end);exit()
+def yes(f=True): printe("Yes") if (f) else None
+def no(f=True): printe("No") if (f) else None
+def listr(l:list,f=str): return "".join(list(map(f,l)))
+def debug(*values,sep=" ",end="\n"): print(*values,sep=sep,end=end,file=sys.stderr)
+def look(pos:tuple,board:list): return board[pos[0]][pos[1]]
+def printYN(f:bool): yes() if f else no()
+if __name__=="__main__":
+    main()
