fb0109a7da726765b8e1146517aded9e734a88e2
Lit-to <litto0315@gmail.com>
2025-06-06 07:42:17 +0900

    ABC268B AC

---
+++ contest/ABC349/ABC349D.py
@@ -0,0 +1,74 @@
+# ABC349D Divide Interval
+
+# 関数定義スペース
+def calc_msb(num:int):
+    """
+    MSbを求める。
+    本来はここをO(logN)で求める方法があるはずだが、ここでは簡便にO(N)で求める。
+    ※要修正
+    """
+    num-=1
+    msb=0
+    while num>0:
+        msb+=1
+        num>>=1
+    return msb
+
+def search(ok:int,ng:int,f:bool)->int: #二分探索原型
+    # okは条件を満たす領域の外側
+    # ngは条件を満たさない領域の外側
+    # fは条件を満たすかどうかの評価関数
+        # lambda i:a[i]<x xを含まない最大のiを返す
+        # lambda i:a[i]<=x xを含む最大のiを返す
+    while 1<abs(ok-ng):
+        mid=(ng+ok)//2
+        if f(mid):
+            ok=mid
+        else:
+            ng=mid
+    return ok
+
+
+def main():
+    # 入力スペース ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Lit_to
+    L, R = map(int,input().split())
+    # 処理スペース ================================================================================================Lit_to
+
+    msb=calc_msb(R)
+    l,r=L,R
+    while l<R:
+        search(l,r,lambda x: l<=x and x<r)
+
+
+
+
+
+
+
+
+# 以下コピペ部分=============================================================
+# インポート(本編はだいたい30行目あたり)
+import sys,itertools,math,heapq
+from collections import defaultdict,deque
+from sortedcontainers import SortedSet, SortedList, SortedDict # CPython?
+# pypyjit.set_param('max_unroll_recursion=-1')
+sys.setrecursionlimit(10**8)
+sys.set_int_max_str_digits(0)
+dict=defaultdict
+# 便利定数定義
+ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# ALPHABET="abcdefghijklmnopqrstuvwxyz"
+MOD=998244353
+MAX=10**18
+LRUD=[(0,1),(0,-1),(1,0),(-1,0)]
+LURULDRD=[(-1,-1),(-1,1),(1,-1),(1,1)]
+# 便利関数定義
+def input(): return (sys.stdin.readline()).rstrip()
+def printe(*values,sep=" ",end="\n"):print(*values,sep=sep,end=end);exit()
+def yes(f=True): printe("Yes") if (f) else None
+def no(f=True): printe("No") if (f) else None
+def listr(l:list,f=str): return "".join(list(map(f,l)))
+def debug(*values,sep=" ",end="\n"): print(*values,sep=sep,end=end,file=sys.stderr)
+def printYN(f:bool): yes() if f else no()
+if __name__=="__main__":
+    main()
