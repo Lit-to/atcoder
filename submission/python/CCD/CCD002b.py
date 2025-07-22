fb5cad6a789097ce9e94188c0ff198a85308e26c
Lit-to <litto0315@gmail.com>
2025-07-22 23:54:01 +0900

    add:1完

---
+++ submission/python/CCD/CCD002b.py
@@ -0,0 +1,85 @@
+# CCD002b
+# ABC149D
+# 2025-07-22 21:57:20
+def main():
+    # 関数定義スペース
+
+    def func():
+        ...
+
+    ...    
+    # 入力スペース
+
+    N, K = map(int,input().split())
+    R, S, P = map(int,input().split())
+    T = list(input())
+    rsp={"r":R,"s":S,"p":P}
+    win={"r":"p","s":"r","p":"s"}
+    avoided = []
+    ...
+
+    # 処理スペース
+
+    dp = [] # (R S P)
+    for i in range(N):
+        dp.append(rsp[T[i]])
+
+
+    result = 0
+    for i in range(N):
+        result += dp[i]
+        avoided.append(False)
+        if 0 <= i-K and T[i-K] == T[i] and avoided[i-K]==False:
+            result-=rsp[T[i]]
+            avoided[-1]=True
+    print(result)
+
+    ...
+
+
+
+# テストケース中枢処理
+def case():
+    TESTCASE = 1
+    # TESTCASE = int(input()) # テストケース数の指定
+    for _ in range(TESTCASE):
+        try:
+            main()
+        except solvedException:
+            continue
+        # raise unSolvedExeption
+
+
+# インポート
+import sys, itertools, math, heapq
+from collections import defaultdict, deque
+from sortedcontainers import SortedSet, SortedList, SortedDict  # CPython?
+
+# 定数・環境設定
+sys.setrecursionlimit(10**8)
+sys.set_int_max_str_digits(0)
+dict = defaultdict
+UPPER_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LOWER_ALPHABET="abcdefghijklmnopqrstuvwxyz"
+MOD = 998244353
+MAX = 10**18
+LRUD = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+LURULDRD = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+# 便利関数定義(超圧縮)
+def input(): return (sys.stdin.readline()).rstrip()
+def printe(*values,sep=" ",end="\n"):print(*values,sep=sep,end=end); fin()
+def yes(f=True): printe("Yes") if (f) else None
+def no(f=True): printe("No") if (f) else None
+def debug(*values,sep=" ",end="\n"): print(*values,sep=sep,end=end,file=sys.stderr)
+def printYN(f:bool): yes() if f else no()
+def fin(f=True): raise solvedException if f else None
+
+# 例外クラス
+class solvedException(Exception): pass # 処理打ち切り例外
+class unSolvedExeption(Exception): # 回答未出力例外
+    def __init__(description = "解答が出力されていません。"): super().__init__(description)
+
+# 実行
+if __name__ == "__main__":
+    case()
