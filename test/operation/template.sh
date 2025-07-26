f01763513e50a00ddec44704f1c1fdf30c00b2d7
Lit-to <litto0315@gmail.com>
2025-07-26 18:09:40 +0900

    update:ファイルパス更新

--- test/operation/template.sh
+++ test/operation/template.sh
@@ -1,13 +1,13 @@
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 pushd "$SCRIPT_DIR" > /dev/null
 
-cp "../../lang/python/material/template.py" "../../TEMPLATE/a.py"
-cp "../../lang/python/material/template.py" "../../TEMPLATE/b.py"
-cp "../../lang/python/material/template.py" "../../TEMPLATE/c.py"
-cp "../../lang/python/material/template.py" "../../TEMPLATE/d.py"
-cp "../../lang/python/material/template.py" "../../TEMPLATE/e.py"
-cp "../../lang/python/material/template.py" "../../TEMPLATE/f.py"
-cp "../../lang/python/material/template.py" "../../TEMPLATE/g.py"
+cp "../../lang/python/material/template.py" "../../lang/python/contest/a.py"
+cp "../../lang/python/material/template.py" "../../lang/python/contest/b.py"
+cp "../../lang/python/material/template.py" "../../lang/python/contest/c.py"
+cp "../../lang/python/material/template.py" "../../lang/python/contest/d.py"
+cp "../../lang/python/material/template.py" "../../lang/python/contest/e.py"
+cp "../../lang/python/material/template.py" "../../lang/python/contest/f.py"
+cp "../../lang/python/material/template.py" "../../lang/python/contest/g.py"
 
 popd > /dev/null
 
