1adbb2de827a30abe3d247fc79c17a9457653736
Lit-to <litto0315@gmail.com>
2025-09-27 10:24:19 +0000

    sampleAの更新

--- test/monkey/sampleA.py
+++ test/monkey/sampleA.py
@@ -1,135 +1,55 @@
-# ABC421c
-# 2025-08-30 20:58:28
-
-def main():
-    # 関数定義スペース
-    def search(ok:int,ng:int,f:bool)->int:
-        """二分探索を行う関数
-        単調増加の範囲においてokのうちいちばんngに近いものの値を返す
-        利用例:
-        -  lambda i:a[i]<x xを含まない最大のiを返す
-        -  lambda i:a[i]<=x xを含む最大のiを返す
-    
-        Args:
-            -  ok (int): 評価関数fに渡したときに必ずTrueを返すことが保証されている値
-            -  ng (int): 評価関数fに渡したときに必ずFalseを返すことが保証されている値
-            -  f (bool): 評価関数(引数1/戻り値bool)
-    
-        Returns:
-            -  int: 結果
-        """
-        while 1<abs(ok-ng):
-            mid=(ng+ok)//2
-            if f(mid):
-                ok=mid
-            else:
-                ng=mid
-        return ok
-
-    def func():
-        ...
-
-    ...    
-    # 入力スペース
-
-    N = splitN(input())
-    S = list(input())
-    A = []
-    B = []
-    BA = []
-    AB = []
-    for i in range(2*N):
-        if S[i]=="A":
-            A.append(i)
-        elif S[i]=="B":
-            B.append(i)
-    for i in range(N):
-        if A[i]<B[i]:
-            AB.append(abs(i-search(N-1,-1,lambda x:A[i]<B[x])))
-            BA.append(abs(i-search(N-1,-1,lambda x:B[i]<A[x])))
-        elif B[i]<A[i]:
-            AB.append(abs(search(0,N,lambda x:B[x]<A[i])+1-i))
-            BA.append(abs(search(0,N,lambda x:A[x]<B[i])+1-i))
-        pass
-    debug(*AB)
-    debug(*BA)
-    print(min(sum(AB),sum(BA)))
-    ...
-
-    # 処理スペース
-
-
-
-
-
-
-    ...
-
-
-
-# テストケース中枢処理
-def case():
-    TESTCASE = 1
-    # TESTCASE = int(input()) # テストケース数の指定
-    for _ in range(TESTCASE):
-        try:
-            main()
-        except solvedException:
-            continue
-        # raise unSolvedExeption
-
-
-# インポート
-import sys, itertools, math, heapq
-from collections import defaultdict, deque
-from sortedcontainers import SortedSet, SortedList, SortedDict  # CPython?
-
-# 定数・環境設定
+# インポート(本編はだいたい30行目あたり)
+import sys,itertools,math,heapq
+from collections import defaultdict,deque
+from sortedcontainers import SortedSet, SortedList, SortedDict #Cpythonでは動かない(importにも多少時間がかかる)
 sys.setrecursionlimit(10**8)
-sys.set_int_max_str_digits(0)
-dict = defaultdict
-UPPER_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-LOWER_ALPHABET="abcdefghijklmnopqrstuvwxyz"
-MOD = 998244353
-MAX = 10**18
-LRUD = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-LURULDRD = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+dict=defaultdict
 
-# 各種関数定義(超圧縮)
-## 便利関数
-def printe(*values,sep=" ",end="\n"):print(*values,sep=sep,end=end); fin()
-def prin(stop=False,sep=" ",end="\n",file=sys.stdout):return (lambda*values:printe(*values,sep=sep,end=end)) if stop else (lambda*values:print(*values,sep=sep,end=end,file=file))
-def yes(f=True): printe("Yes") if (f) else None
-def no(f=True): printe("No") if (f) else None
-def debug(*values,sep=" ",end="\n"): print(*values,sep=sep,end=end,file=sys.stderr)
+# 便利定数定義
+ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# ALPHABET="abcdefghijklmnopqrstuvwxyz"
+MOD=998244353
+# MAX=10**9+1
+
+# 便利関数定義
+def input(): return (sys.stdin.readline()).rstrip()
+def yes(): print("Yes"); exit()
+def no(): print("No"); exit()
+def printe(*values: object,sep: str | None = " ",end: str | None = "\n",): print(*values,sep=sep,end=end); exit() #Cpythonでは動かない
+def listr(l:list): return "".join(l)
+def debug(*values: object,sep: str | None = " ",end: str | None = "\n",): print(*values,sep=sep,end=end,file=sys.stderr) #デバッグ出力用
 def printYN(f:bool): yes() if f else no()
-def fin(f=True): raise solvedException if f else None
-## 分割関数
-def split(value:str|list,sep:str=" ",func:callable=str) -> list:
-    result = []
-    if type(value) == list:
-        for i in range(len(value)):
-            result.append(split(value[i],sep))
-        return result
-    else:
-        if sep in value:
-            for i in value.split(sep):
-                result.append(func(i))
+
+# 関数定義スペース
+def search(ok:int,ng:int,f:bool):
+    while 1<abs(ok-ng):
+        mid=(ok+ng)//2
+        if f(mid):
+            ok=mid
         else:
-            result.append(func(value))
-        return result
-## 入力受け取り用
-def input():return(sys.stdin.readline()).rstrip() #入力定数倍
-def splitS(value:str|list,sep:str=" ")->str:return split(value,sep)[0] #文字列・分割して最初
-def splitN(value:str|list,sep:str=" ")->int:return split(value,sep,int)[0] # 整数・分割して最初
-def splitB(value:str|list,sep:str=" ")->list:return list(split(value,sep)) # 文字列・分割してすべて
-def splitA(value:str|list,sep:str=" ")->list:return list(split(value,sep,int)) # 整数・分割してすべて
+            ng=mid
+    return ok
+def calcMoney(A:list,X:int):
+    result=0
+    for i in A:
+        result+=min(i,X)
+    return result
+# Lit_to~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#入力スペース
+# S = list(input())
+# S = input() 
+# N = int(input())
+N, M = map(int,input().split())
+A = list(map(int,input().split()))
+# BOARD = [list(input()) for i in range(H)]#文字列のリスト
+# BOARD = [list(map(int,input().split())) for i in range(H)]#数値のリスト
+# A = map(lambda x:x,sorted(list(map(int,(input().split())))))#数の組み合わせを並び替えて渡す
+# Lit_to================================================================================================#処理スペース
 
-# 例外クラス
-class solvedException(Exception): pass # 処理打ち切り例外
-class unSolvedExeption(Exception): # 回答未出力例外
-    def __init__(description = "解答が出力されていません。"): super().__init__(description)
+if sum(A)<=M:
+    printe("infinite")
+result=search(-1,max(A)+1,lambda x: calcMoney(A,x)<=M)
+if max(A)<=result:
+    print("infinite")
+else:
+    print(result)
 
-# 実行
-if __name__ == "__main__":
-    case()
