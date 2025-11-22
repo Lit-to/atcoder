1145955a3b2b2a43e38b38749a374f3c498de223
Lit-to <litto0315@gmail.com>
2025-11-22 16:49:19 +0000

    fix:移動

--- start.sh
+++ start.sh
@@ -1,13 +1,26 @@
 # 今のフォルダを変数に保存  
 OLD_DIR="$PWD"
 
-# 移動する  
+# フォルダ名の決定
 cd ~/atcoder/
-
 echo "コンテスト名を入力=>"
 read contestName
-mkdir submission/ABC/$contestName
+contestName=$(echo "$contestName" | tr '[:lower:]' '[:upper:]')
+echo "この名前でコンテストを開始します=>\"$contestName\" y/n"
+read isOK 
+if [ "$contestName" != "y" ] && [ "$contestName" != "yes" ]
+    exit 0
+fi
 
+# gitの更新
+
+git fetch -p
+git checkout main
+git pull
+git checkout -b $contestName main
+
+# フォルダの作成･ファイルの移動
+mkdir submission/ABC/$contestName
 for letter in A B C D E F G; do
     cp material/template.py submission/ABC/$contestName/$contestName$letter.py
     done
