240fcf6212a624633cc6fa75a55d87819ffa31bc
Lit-to <litto0315@gmail.com>
2025-12-20 15:37:32 +0000

    add:AC :-   ABC437C.cpp -   ABC437C.py -   ABC437B.cpp -   ABC437D.py -   ABC437G.py -   ABC437B.py -   ABC437A.cpp -   ABC437F.py -   ABC437A.py -   ABC437E.py

---
+++ submission/ABC/ABC437/ABC437A_1220.cpp
@@ -0,0 +1,24 @@
+// ABC437A
+// template
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+
+int main()
+{
+
+    /*入力エリア*/
+    // int N;
+    // std::cin >> N;
+
+    int A;
+    int B;
+    std::cin >> A >> B;
+
+    std::cout << A * 12 + B << std::endl;
+
+    /*実装エリア */
+
+    return 0;
+}
