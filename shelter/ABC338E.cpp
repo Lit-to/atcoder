87f9d2a21a1143c46b600e97657035b5aa71490e
Lit-to <litto0315@gmail.com>
2025-12-27 14:10:42 +0000

    ABC438終了後コミット

---
+++ shelter/ABC338E.cpp
@@ -0,0 +1,52 @@
+// ABC338E
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+
+/**
+ *方針メモ欄
+ *
+ * # お気持ち
+ *
+ * # 入力
+ *
+ * # 事前準備(宣言周り,~を求めておく系のやつ)
+ *
+ * # 具体的なロジック
+ *
+ *
+ *
+ *
+ *
+ * # 出力
+ *
+ *
+ */
+
+int main()
+{
+
+    /*入力エリア*/
+    int64_t N;
+    std::cin >> N;
+
+    int64_t H;
+    int64_t W;
+    std::cin >> H >> W;
+
+    std::vector<int64_t> A(N);
+    for (int64_t i = 0; i < N; ++i)
+    {
+        std::cin >> A[i];
+    }
+
+    std::string S;
+    std::cin >> S;
+
+    std::cout << S << std::endl;
+
+    /*実装エリア */
+
+    return 0;
+}
