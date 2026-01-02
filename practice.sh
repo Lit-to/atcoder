525bc1e43654c0ff9234c040ccff11a9dee66507
Lit-to <litto0315@gmail.com>
2026-01-02 09:56:22 +0000

    mv:フォルダ名と構成を変更

--- practice.sh
+++ practice.sh
@@ -20,7 +20,7 @@ else
     fileName="${basename}.${ext}"
 fi
 
-template="material/template.${ext}"
+template="modules/template.${ext}"
 if [ ! -f "$template" ]; then
     echo "対応するテンプレートファイルが見つかりません: $template" >&2
     exit 1
