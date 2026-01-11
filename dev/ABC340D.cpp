52a220bf5d784943b9c18fc937524744a6acd140
Lit-to <litto0315@gmail.com>
2026-01-11 13:54:40 +0000

    やり途中

--- dev/ABC340D.cpp
+++ dev/ABC340D.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <cstdint>
 #include <map>
+#include <queue>
+#include <set>
 
 /**
  *方針メモ欄
@@ -24,7 +26,11 @@
  *
  *
  */
-
+std::vector<int64_t> costs;
+bool eval(int64_t &lhs, int64_t &rhs)
+{
+    return costs[lhs] < costs[rhs];
+}
 int main()
 {
 
@@ -32,20 +38,38 @@ int main()
     std::cin >> N;
     struct EDGE
     {
-        int64_t to;
-        int64_t cost;
+        int64_t m_to;
+        int64_t m_cost;
         bool operator<(const EDGE &rhs) const
         {
-            return true;
+            return m_cost > rhs.m_cost;
         }
     };
     std::vector<std::vector<EDGE>> GRAPH(N);
-    for (int64_t i = 0; i < N; ++i)
+    for (int64_t i = 0; i < N - 1; ++i)
     {
         int64_t a, b, x;
         std::cin >> a >> b >> x;
-        GRAPH[i].push_back(EDGE{.to = i + 1, .cost = a});
-        GRAPH[i].push_back(EDGE{.to = x, .cost = b});
+        GRAPH[i].push_back(EDGE{.m_to = i + 1, .m_cost = a});
+        GRAPH[i].push_back(EDGE{.m_to = x - 1, .m_cost = b});
+    }
+
+    std::priority_queue<int64_t> tasks;
+    tasks.push(0);
+
+    while (!tasks.empty())
+    {
+        EDGE pos = tasks.top();
+        if (pos.m_to == N - 1)
+        {
+            std::cout << pos.m_cost << std::endl;
+            return 0;
+        }
+        tasks.pop();
+        for (EDGE i : GRAPH[pos.m_to])
+        {
+            tasks.push(i.m_to);
+        }
     }
 
     return 0;
