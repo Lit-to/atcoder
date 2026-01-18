9fe724e9e7ae2aae297f61403a39154965832cfa
Lit-to <litto0315@gmail.com>
2026-01-18 04:28:04 +0000

    refactor: pythonをcppに合わせて更新

--- modules/py/union_find.py
+++ modules/py/union_find.py
@@ -4,63 +4,55 @@ class unionFind:
     頂点を結合した際に連結する要素の親ノードを1つに統一する
     各頂点の親ノードを高速に返す
     """
-    def __init__(self,n:int):
+    def __init__(self,length:int):
         """
         コンストラクタ
 
         Args:
             -  n (int):すべて独立したn個ノードとして初期化する
         """
-        self.__par=[-1]*n
-        self.__length = n
+        self.parent=[-1]*length
+        self.size = length
 
-    def root(self,i:int)->int:
-        """親ノードを再帰的に探索する
-        途中で見つけた親ノードで各子ノード更新する
-        Args:
-            -  i (int):探したいノード 
-
-        Returns:
-            -  int:親ノード
-        """
-        if self.__par[i]<0:
-            return i
-        else:
-            self.__par[i]=self.root(self.__par[i])
-            return self.__par[i]
-
-    def unite(self,a:int,b:int)->None:
+    def unite(self,node_A:int,node_B:int)->None:
         """ノードaとノードbを結合する
 
         Args:
             -  a (int): 結合したいノード
             -  b (int): 結合したいノード
         """
-        b,a=self.root(b),self.root(a)
-        if a==b:
+        node_B,node_A=self.__update_root(node_B),self.__update_root(node_A)
+        if node_A==node_B:
             return
-        if b<a:
-            self.__par[a] += self.__par[b]
-            self.__par[b] = a
+        if node_B<node_A:
+            self.parent[node_A] += self.parent[node_B]
+            self.parent[node_B] = node_A
         else:
-            self.__par[b] += self.__par[a]
-            self.__par[a] = b
+            self.parent[node_B] += self.parent[node_A]
+            self.parent[node_A] = node_B
     
-    def update(self):
+    def fetch_size(self,node:int)->int:
         """
-        update関数
-        すべての親を更新し、親配列の中身を返す(遅いため注意)
+        ノードの属するグループのサイズを求める
+        Args:
+        -   ノード番号
         """
-        result = []
-        for i in range(self.__length):
-            result.append(self.root(i))
-        return result
+        return self.size[self.__update_root(node)]
 
-    def __len__(self):
+
+    def __update_root(self,i:int)->int:
+        """親ノードを再帰的に探索する
+        途中で見つけた親ノードで各子ノード更新する
+        Args:
+            -  i (int):探したいノード 
+        Returns:
+            -  int:親ノード
         """
-        サイズ取得関数
-        """
-        return self.__length
+        if self.parent[i]<0:
+            return i
+        else:
+            self.parent[i]=self.__update_root(self.parent[i])
+            return self.parent[i]
 
 
 
