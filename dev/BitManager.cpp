d91c83be6718fb983e8999b3931b2d148ebc384d
Lit-to <litto0315@gmail.com>
2026-02-03 15:56:03 +0000

    doc:doxygenを追加

--- dev/BitManager.cpp
+++ dev/BitManager.cpp
@@ -1,19 +1,31 @@
 #include <vector>
 #include <cstdint>
 
+/**
+ * @class
+ * @brief ビットマネージャー
+ */
 class BitManager
 {
 public:
+    /**
+     * @brief コンストラクタ
+     * @param ビット列桁数
+     */
     BitManager(int64_t digits)
     {
         m_max = 1 << digits;
         m_digits = digits;
         m_value = 0;
     }
-    std::vector<int> next()
+    /**
+     * 次のビット列を取得する
+     * @return ビット列
+     */
+    std::vector<int64_t> next()
     {
         int64_t value = m_value;
-        std::vector<int> result(m_digits);
+        std::vector<int64_t> result(m_digits);
         for (int64_t i = 0; i < m_digits; ++i)
         {
             result[m_digits - i - 1] = (value & 1);
@@ -22,11 +34,15 @@ public:
         ++m_value;
         return result;
     }
+    /**
+     * 終了済みかどうかを取得する
+     * @return 終了済みかどうか
+     */
     bool isFinished()
     {
         return m_max <= m_value;
     }
-    int64_t m_max;
-    int64_t m_value;
-    int64_t m_digits;
+    int64_t m_max;    //! 桁数最大値
+    int64_t m_value;  //! 現在の値
+    int64_t m_digits; //! 桁数
 };
