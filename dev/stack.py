d9c5d7553d784e23e45cc30f26fb9009cb422aa9
Lit-to <litto0315@gmail.com>
2026-02-23 16:17:25 +0000

    スタックの作成

---
+++ dev/stack.py
@@ -0,0 +1,27 @@
+class stack:
+
+    def __init__(self):
+        self.data = []
+        self.begin = 0
+        self.end = 0
+
+    def __len__(self):
+        return self.end - self.begin
+
+    def __str__(self):
+        return str(self.data[self.begin:self.end])
+
+    def isEmpty(self):
+        return 0<len(self)
+
+    def push(self,value):
+        if len(self.data) < self.end:
+            self.data.append(-1)
+        self.data[self.end] = value
+        self.end += 1
+
+    def pop(self):
+        self.end -= 1
+    
+    def top(self):
+        return self.data[self.end - 1]
