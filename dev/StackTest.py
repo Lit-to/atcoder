d3b9aca03b7319b311254598a1488c86b4e850cc
Lit-to <litto0315@gmail.com>
2026-02-25 16:34:02 +0000

    コンテストコードでは対応できなくなったのでテストコードを追加

--- dev/StackTest.py
+++ dev/StackTest.py
@@ -1,3 +1,4 @@
+# 2026-02-25 16:15:43
 class Stack:
 
     def __init__(self):
@@ -37,3 +38,36 @@ class Stack:
         self.__data = self.__data[self.__begin:self.__end]
         self.__begin = 0
         self.__end = len(self.__data)
+
+import random
+
+litStack = Stack()
+stlStack = []
+testList = [0,0,0,0,0]
+for i in range(2*10**5):
+    query = random.randint(0,4)
+    testList[query] += 1
+    if query==0:
+        r = random.randint(0,10**9)
+        litStack.Push(r)
+        stlStack.append(r)
+    elif query==1:
+        assert len(litStack)==len(stlStack)
+        if len(stlStack)!=0:
+            litStack.Pop()
+            stlStack.pop()
+    elif query==2:
+        assert len(litStack)==len(stlStack)
+        if len(stlStack)!=0:
+            r = random.randint(0,len(stlStack)-1)
+            assert litStack[r]==stlStack[r]
+    elif query==3:
+        assert litStack.IsEmpty() == (len(stlStack) == 0)
+    else:
+        litStack.Rebuild()
+    assert str(litStack) == str(stlStack)
+print("OK")
+print("Log:",testList)
+
+
+
