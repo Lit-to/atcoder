ced5b479bef6b88e902a364fb1e3d78aa83c1371
Lit-to <litto0315@gmail.com>
2026-02-25 16:01:08 +0000

    fix:プロパティをすべてプライベートに変更

--- dev/Queue.py
+++ dev/Queue.py
@@ -1,35 +1,35 @@
 class Queue:
 
     def __init__(self):
-        self.data = []
-        self.begin = 0
-        self.end = 0
+        self.__data = []
+        self.__begin = 0
+        self.__end = 0
 
     def __len__(self):
-        return self.end - self.begin
+        return self.__end - self.__begin
 
     def __str__(self):
-        return str(self.data[self.begin:self.end])
+        return str(self.__data[self.__begin:self.__end])
     
     def __getitem__(self, key):
-        if not self.begin+key < self.end:
+        if not self.__begin+key < self.__end:
             raise IndexError
-        return self.data[self.begin+key]
+        return self.__data[self.__begin+key]
 
     def IsEmpty(self):
         return len(self)==0
 
     def Enqueue(self,value):
-        self.data.append(value)
-        self.end += 1
+        self.__data.append(value)
+        self.__end += 1
 
     def Dequeue(self):
         if self.IsEmpty() == False:
             raise IndexError("list index out of range") 
-        self.begin += 1
+        self.__begin += 1
     
     def GetFront(self):
-        return self.data[self.begin]
+        return self.__data[self.__begin]
 
     def Rebuild(self):
-        self.data = self.data[self.begin:self.end]
+        self.__data = self.__data[self.__begin:self.__end]
