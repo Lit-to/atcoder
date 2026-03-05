d9d104a5c562c9fc99daafe604c1492e85755ebf
Lit-to <litto0315@gmail.com>
2026-03-05 15:48:52 +0000

    merge:Litutilityに合流

--- modules/py/LitUtility.py
+++ modules/py/LitUtility.py
@@ -1,3 +1,6 @@
+import math
+
+
 class LitUtility:
     def Search(ok:int,ng:int,f:bool)->int:
         """二分探索を行う関数
@@ -54,3 +57,92 @@ class LitUtility:
                 ng = mid
         else:
             return ok
+    def Factorial(n: int) -> int:
+        """
+        n!の結果を返す。
+
+        より具体的には整数nの階乗の値を返す。
+
+        Args:
+            -  n(int):nの値
+
+        Returns:
+            -  int: 計算結果
+        """
+        return LitUtility.Permutation(n, n)
+
+    def Permutation(n: int, k: int) -> int:
+        """
+        nPkの値を返す。
+        より具体的にはn個の整数からk個を順番に並べられる通り数を返す。
+
+        Args:
+            -  n(int):nの値
+            -  k(int):kの値
+
+        Returns:
+            -  int: 計算結果
+        """
+        result = 1
+        for i in range(n, n - k, -1):
+            result *= i
+        return result
+
+    def Combination(n: int, k: int) -> int:
+        """
+        nCkの値を返す。
+        より具体的にはn個の整数からk個を選ぶ組み合わせ数を返す。
+
+        Args:
+            -  n(int):nの値
+            -  k(int):kの値
+
+        Returns:
+            -  int: 計算結果
+        """
+        return LitUtility.Permutation(n, k) // LitUtility.Factorial(k)
+
+    def CalcGCD(value_a: int, value_b: int) -> int:
+        """互除法で最大公約数を求める関数
+
+        Args:
+            -  value_a (int): 求めたい値A
+            -  value_b (int): 求めたい値B
+
+        Returns:
+            -  int: AとBの最大公約数
+        """
+        if value_a < value_b:
+            value_a, value_b, value_b, value_a
+        while value_b != 0:
+            temp = value_b
+            value_b = value_a % value_b
+            value_a = temp
+        return value_a
+
+    def Prime(num:int) -> list:
+        """素因数分解の結果を返す
+        ※利用するためには import mathが必須
+
+        Args:
+            -  num (int): 合成数または素数
+
+        Returns:
+            -  list: 分解した素数を返す
+        """
+        result=[]
+        while num%2==0:
+            num//=2
+            result.append(2)
+        c=3
+        while c<=math.sqrt(num):
+            if num%c==0:
+                num//=c
+                result.append(c)
+            else:
+                c+=2
+        if num!=1:
+            result.append(num)
+        return result
+
+
