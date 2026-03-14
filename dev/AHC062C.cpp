812648bbe5e4d230e722caf3a644dcd71cd9f564
Lit-to <litto0315@gmail.com>
2026-03-14 05:56:06 +0000

    AHC062コンテスト開始前コミット

---
+++ dev/AHC062C.cpp
@@ -0,0 +1,22 @@
+// AHC062C
+// template
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+
+/**
+ *方針メモ欄
+ *
+ * # お気持ち
+ *
+ * # 具体的なロジック
+ *
+ */
+
+/*=========================*/
+
+int main()
+{
+}
