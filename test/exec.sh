74dea5a03beb6272015aa96150783221c9d4f948
Lit-to <litto0315@gmail.com>
2026-03-20 10:06:37 +0000

    AC:TESOK001M.cpp

--- test/exec.sh
+++ test/exec.sh
@@ -1,26 +1,35 @@
 #!/usr/bin/env bash
 #================================
 # ここに実行コマンドを書く場所
-execCommand="../dev/a"
+execCommand="../dev/a" #Cpp
+# execCommand="pypy3 script.py" #Python
 
 #================================
 
 set -euo pipefail
 pushd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" > /dev/null
-mkdir output
-output="./output/$(date +%Y%m%d%H%M%S%3N)"
-mkdir $output
-RESULT=$output/RESULT
+output="./output"
+mkdir -p $output
+RESULT="$output/$(date +%Y%m%d%H%M%S%3N)"
 touch $RESULT
-echo $execCommand > $RESULT
+echo "Execute Result => $execCommand" > $RESULT
 for file in ./input/*; do
     [ -e "$file" ] || continue
     name=$(basename "$file")
+    echo "file : $file" >> $RESULT
+    echo -e "================\n" >> $RESULT
     start=$(date +%s%N)
-    $execCommand < $file > $output/$name
+    $execCommand < $file >> $RESULT
     end=$(date +%s%N)
     path=$(realpath "$output/$name")
-    echo $(( (end - start) / 1000000 ))"ms => $path" >> $RESULT
+    time=$(( (end - start) / 1000000 ))
+    echo -e "\n================" >> $RESULT
+    if [ "$time" -gt 2000 ]; then
+        echo "[TLE] time: $time ms" >> $RESULT
+    else
+        echo "[---] time: $time ms" >> $RESULT
+    fi
 done
-cat $RESULT
+resultPath=$(realpath "$RESULT")
+echo "Test Summary => " $resultPath
 popd > /dev/null
