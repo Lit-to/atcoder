a73529a48bf5ea991e924be6f3968733bcc03aa6
Lit-to <litto0315@gmail.com>
2026-04-08 12:36:43 +0000

    AC:ABC213D.cpp

---
+++ submission/ABC/ABC213/ABC213D_0408.cpp
@@ -0,0 +1,47 @@
+// ABC213D
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+//
+int main()
+{
+    int64_t N;
+    std::cin >> N;
+    std::vector<std::vector<int64_t>> GRAPH(N);
+    for (int64_t i = 0; i < N - 1; ++i)
+    {
+        int64_t a, b;
+        std::cin >> a >> b;
+        GRAPH[a - 1].push_back(b - 1);
+        GRAPH[b - 1].push_back(a - 1);
+    }
+    for (int64_t i = 0; i < N; ++i)
+    {
+        std::sort(GRAPH[i].begin(), GRAPH[i].end());
+    }
+    std::vector<bool> done(N);
+    auto dfs = [&](auto &&self, int64_t prev, int64_t node) -> void
+    {
+        std::cout << node + 1 << " ";
+        for (int64_t &nextTo : GRAPH[node])
+        {
+            if (nextTo == prev)
+            {
+                continue;
+            }
+            self(self, node, nextTo);
+            std::cout << node + 1 << " ";
+        }
+    };
+    dfs(dfs, -1, 0);
+    std::cout << std::endl;
+}
+
+//======================
+/**
+ *方針メモ欄
+ *
+ */
+//======================
