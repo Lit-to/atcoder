a5b34ed043c58796d123356ce89c76d1372e6776
Lit-to <litto0315@gmail.com>
2026-04-23 11:03:58 +0000

    AWC0054コンテスト開始前コミット

---
+++ dev/AWC0054E.cpp
@@ -0,0 +1,38 @@
+// AWC0054E
+// template
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+//
+
+/**
+ * 1ケースぶんの処理実行
+ */
+void solve()
+{
+}
+
+/**
+ * エントリポイント
+ * テストケースごとに回す(デフォルトは1)
+ */
+int main()
+{
+    int64_t TESTCASES = 1;
+
+    // std::cin >> TESTCASES;
+
+    for (int64_t i = 0; i < TESTCASES; ++i)
+    {
+        solve();
+    }
+}
+
+//======================
+/**
+ *方針メモ欄
+ *
+ */
+//======================
