a897220a2a6ac743339d3c2a1c3b65366432c056
Lit-to <litto0315@gmail.com>
2026-04-29 16:23:49 +0000

    ダイクストラつくった

---
+++ modules/cpp/Dijkstra.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <cstdint>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+template <typename T>
+using GreaterPriorityQueue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+class Dijkstra
+{
+public:
+    struct EDGE
+    {
+        int64_t to;
+        int64_t cost;
+        bool operator>(const EDGE &rhs) const
+        {
+            return cost > rhs.cost;
+        };
+    };
+    Dijkstra(const std::vector<std::vector<EDGE>> &GRAPH)
+    {
+        m_graph = GRAPH;
+        m_graphSize = m_graph.size();
+    }
+    std::vector<int64_t> calcCosts(int64_t start)
+    {
+        int64_t INF = INT64_MAX / 2;
+        std::vector<int64_t> costs(m_graph.size(), INF);
+        costs[start] = 0;
+        GreaterPriorityQueue<EDGE> gpq;
+        gpq.push({.to = start, .cost = costs[start]});
+        while (!gpq.empty())
+        {
+            EDGE pos = gpq.top();
+            gpq.pop();
+            if (costs[pos.to] < pos.cost)
+            {
+                continue;
+            }
+            for (const EDGE &nextNode : m_graph[pos.to])
+            {
+                int64_t nextCost = pos.cost + nextNode.cost;
+                if (nextCost < costs[nextNode.to])
+                {
+                    costs[nextNode.to] = nextCost;
+                    gpq.push({.to = nextNode.to, .cost = nextCost});
+                }
+            }
+        }
+        return costs;
+    }
+    std::vector<std::vector<EDGE>> m_graph;
+    int64_t m_graphSize;
+};
--- modules/test/DijkstraTest.cpp
+++ modules/test/DijkstraTest.cpp
@@ -1,28 +1,84 @@
-#include <cstdint>
 #include <iostream>
-#include <vector>
-#include "../../dev/Dijkstra.cpp"
+#include "../cpp/RandomGenerator.cpp"
+#include "../cpp/Dijkstra.cpp"
+
+std::vector<int64_t> dfsDijkstra(std::vector<std::vector<Dijkstra::EDGE>> &GRAPH, int64_t start)
+{
+    std::vector<int64_t> costs(GRAPH.size(), INT64_MAX / 2);
+    std::vector<bool> done(GRAPH.size(), false);
+
+    auto dfs = [&](auto &self, int64_t node, int64_t cost) -> void
+    {
+        if (cost >= costs[node])
+        {
+            return;
+        }
+        costs[node] = cost;
+        for (int64_t i = 0; i < (int64_t)GRAPH[node].size(); ++i)
+        {
+            auto &e = GRAPH[node][i];
+            if (done[e.to])
+            {
+                continue;
+            }
+            done[e.to] = true;
+            self(self, e.to, cost + e.cost);
+            done[e.to] = false;
+        }
+    };
+
+    done[start] = true;
+    dfs(dfs, start, 0);
+
+    return costs;
+}
+
 int main()
 {
-    int64_t N, M;
-    std::cin >> N >> M;
-    std::vector<std::vector<int64_t>> GRAPH(N);
-    std::vector<int64_t> costs(M);
-
-    for (int64_t i = 0; i < M; ++i)
+    // === テストパラメータ === //
+    const int64_t N_MAX = 1000;
+    const int64_t M_MAX = N_MAX - 1;
+    const int64_t C_MAX = 100;
+    const int64_t TEST_COUNT = 100;
+    // --------------------------
+    randomGenerator rand;
+    // === テストパラメータ === //
+    int64_t test = 0;
+    while (test < TEST_COUNT)
     {
-        int64_t a, b, x;
-        std::cin >> a >> b >> x;
-        GRAPH[a].push_back(b);
-        costs[b] = x;
-    }
-    std::vector<int64_t> result = calctaskCost(0ll, GRAPH, costs);
-    for (int64_t i = 0; i < result.size(); ++i)
-    {
+        int64_t N = rand.Generate(1, N_MAX);
+        int64_t M = rand.Generate(1, M_MAX);
+        std::vector<std::vector<Dijkstra::EDGE>> GRAPH(N);
+        for (int64_t i = 0; i < M; ++i)
         {
-            std::cout << i << " : " << result[i] << std::endl;
+            int64_t u = rand.Generate(0, N);
+            int64_t v = rand.Generate(0, N);
+            if (u == v)
+            {
+                continue;
+            }
+            GRAPH[u].push_back(Dijkstra::EDGE{.to = v, .cost = rand.Generate(0, C_MAX)});
+            GRAPH[v].push_back(Dijkstra::EDGE{.to = u, .cost = rand.Generate(0, C_MAX)});
         }
+        Dijkstra dij(GRAPH);
+
+        int64_t Q = rand.Generate(1, 100);
+        for (int64_t i = 0; i < Q; ++i)
+        {
+            int64_t startNode = rand.Generate(0, N);
+            std::vector<int64_t> dijResult = dij.calcCosts(startNode);
+            std::vector<int64_t> dfsResult = dfsDijkstra(GRAPH, startNode);
+            for (int64_t j = 0; j < dfsResult.size(); ++j)
+            {
+                if (dijResult[j] != dfsResult[j])
+                {
+                    std::cout << "<<<<<<NG" << std::endl;
+                    return 0;
+                }
+            }
+        }
+
+        ++test;
     }
-    std::cout << std::endl;
-    return 0;
+    std::cout << "OK!" << std::endl;
 }
