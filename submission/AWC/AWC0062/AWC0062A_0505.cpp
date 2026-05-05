95eb16d0490be09aa763ea096424c1acffba8459
Lit-to <litto0315@gmail.com>
2026-05-05 16:21:46 +0000

    AC:AWC0062A.cpp

--- submission/AWC/AWC0062/AWC0062A_0505.cpp
+++ submission/AWC/AWC0062/AWC0062A_0505.cpp
@@ -1,3 +1,20 @@
+// AWC0062A
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+#include <queue>
+#include <atcoder/all>
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+using namespace std;
+using ll = int64_t;
+using vll = std::vector<int64_t>;
+using mint = atcoder::modint998244353;
+// using mint = atcoder::modint1000000007;
+template <typename T>
+using greater_priority_queue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
 #include <vector>
 #include <cstdint>
 #include <iostream>
@@ -142,3 +159,44 @@ namespace LitAlgorithm
 
 }
 namespace LitA = LitAlgorithm;
+/**
+ * 1ケースぶんの処理実行
+ */
+void solve()
+{
+    ll N, M, K;
+    cin >> N >> M >> K;
+    vector<ll> A(N);
+    LitA::Input(A);
+    ll p = 0;
+    for (ll i = 0; i < N; ++i)
+    {
+        p += (A[i] / K);
+    }
+    LitA::YesNo(M <= p);
+}
+
+/**
+ * エントリポイント
+ * テストケースごとに回す(デフォルトは1)
+ */
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int64_t TESTCASES = 1;
+
+    // std::cin >> TESTCASES;
+
+    for (int64_t i = 0; i < TESTCASES; ++i)
+    {
+        solve();
+    }
+}
+
+//======================
+/**
+ *方針メモ欄
+ *
+ */
+//======================
