8bf42d676094f714684ea37e7601d85a8d6b6750
Lit-to <litto0315@gmail.com>
2026-06-04 15:17:36 +0000

    AC:TESOK002G.cpp

---
+++ submission/TESOK/TESOK002/TESOK002G_0604.cpp
@@ -0,0 +1,82 @@
+// TESOK002G
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+#include <queue>
+#include <atcoder/all>
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+using namespace std;
+using ll = int64_t;
+using vll = std::vector<int64_t>;
+using mint = atcoder::modint998244353;
+// using mint = atcoder::modint1000000007;
+template <typename T>
+using greater_priority_queue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+/**
+ * 1ケースぶんの処理実行
+ */
+void solve()
+{
+
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    for (ll i = 0; i < N; ++i)
+    {
+        cin >> A[i];
+    }
+    if (N == 2)
+    {
+        if (A[0] == A[1])
+        {
+            cout << "Second" << endl;
+        }
+        else
+        {
+            cout << "First" << endl;
+        }
+        return;
+    }
+    ll result = A[0];
+    for (ll i = 1; i < N; ++i)
+    {
+        result ^= A[i];
+    }
+    if (result == 0)
+    {
+        cout << "Second" << endl;
+    }
+    else
+    {
+        cout << "First" << endl;
+    }
+}
+
+/**
+ * エントリポイント
+ * テストケースごとに回す(デフォルトは1)
+ */
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int64_t TESTCASES = 1;
+
+    // std::cin >> TESTCASES;
+
+    for (int64_t i = 0; i < TESTCASES; ++i)
+    {
+        solve();
+    }
+}
+
+//======================
+/**
+ *方針メモ欄
+ *
+ */
+//======================
