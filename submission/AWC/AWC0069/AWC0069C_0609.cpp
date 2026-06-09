50441cdd0a177d2b104e49c1c346daf05d5e78c4
Lit-to <litto0315@gmail.com>
2026-06-09 14:17:59 +0000

    AC:AWC0069C.cpp

---
+++ submission/AWC/AWC0069/AWC0069C_0609.cpp
@@ -0,0 +1,127 @@
+// AWC0069C
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+#include <queue>
+#include <atcoder/all>
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+using namespace std;
+using ll = int64_t;
+using vll = std::vector<int64_t>;
+using mint = atcoder::modint998244353;
+// using mint = atcoder::modint1000000007;
+template <typename T>
+using greater_priority_queue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+#include <vector>
+#include <cstdint>
+#include <string>
+
+/**
+ * @brief ランレングス圧縮
+ */
+class RunLengthCompression
+{
+public:
+    /**
+     * @brief 1トークンを表す構造体
+     * @details 文字と個数を持つ
+     */
+    struct TOKEN
+    {
+        char code;     //! 文字
+        int64_t count; //! 文字の連続個数
+    };
+    /**
+     * @brief 文字列から連続部分列の個数と文字を持つ配列に圧縮する。
+     * @param target 文字列
+     */
+    static std::vector<TOKEN> Encode(const std::string &target)
+    {
+        std::vector<TOKEN> result(0);
+        result.push_back(TOKEN{.code = target[0], .count = 1});
+        for (int64_t i = 1; i < target.size(); ++i)
+        {
+            if (result.back().code == target[i])
+            {
+                ++result.back().count;
+            }
+            else
+            {
+                result.push_back(TOKEN{.code = target[i], .count = 1});
+            }
+        }
+        return result;
+    }
+    /**
+     * @brief 圧縮済みのものから元の文字列に戻す。
+     * @param target 圧縮済み配列
+     */
+    static std::string Decode(const std::vector<TOKEN> &target)
+    {
+        std::string output;
+        for (int64_t i = 0; i < target.size(); ++i)
+        {
+            for (int64_t j = 0; j < target[i].count; ++j)
+            {
+                output.push_back(target[i].code);
+            }
+        }
+        return output;
+    }
+};
+using RLC = RunLengthCompression;
+/**
+ * 1ケースぶんの処理実行
+ */
+void solve()
+{
+    ll N, K;
+    string S;
+    cin >> N >> K >> S;
+    vector<ll> F(N);
+    ll result = 0;
+    for (ll i = 0; i < N; ++i)
+    {
+        if ((S[i] == '0') ^ (F[i] % 2 == 1))
+        {
+            if (N < i + K)
+            {
+                cout << "-1" << endl;
+                return;
+            }
+            ++result;
+            ++F[i];
+            --F[i + K];
+        }
+        F[i + 1] += F[i];
+    }
+    cout << result << endl;
+}
+
+/**
+ * エントリポイント
+ * テストケースごとに回す(デフォルトは1)
+ */
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int64_t TESTCASES = 1;
+
+    // std::cin >> TESTCASES;
+
+    for (int64_t i = 0; i < TESTCASES; ++i)
+    {
+        solve();
+    }
+}
+
+//======================
+/**
+ *方針メモ欄
+ *
+ */
+//======================
