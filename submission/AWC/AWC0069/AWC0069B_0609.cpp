de0f87dfc804512bb1e42624e8552614d5a27ea9
Lit-to <litto0315@gmail.com>
2026-06-09 13:10:20 +0000

    AC:AWC0069B.cpp

---
+++ submission/AWC/AWC0069/AWC0069B_0609.cpp
@@ -0,0 +1,66 @@
+// AWC0069B
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+#include <queue>
+#include <atcoder/all>
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+using namespace std;
+using ll = int64_t;
+using vll = std::vector<int64_t>;
+using mint = atcoder::modint998244353;
+// using mint = atcoder::modint1000000007;
+template <typename T>
+using greater_priority_queue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+/**
+ * 1ケースぶんの処理実行
+ */
+void solve()
+{
+    ll N, K;
+    cin >> N >> K;
+    vector<ll> FOODS(N);
+    for (ll i = 0; i < N; ++i)
+    {
+        ll t, c;
+        cin >> t >> c;
+        FOODS[i] = t + c;
+    }
+    sort(all(FOODS));
+    ll result = 0;
+    for (ll i = 0; i < K; ++i)
+    {
+        result += FOODS.back();
+        FOODS.pop_back();
+    }
+    cout << result << endl;
+}
+
+/**
+ * エントリポイント
+ * テストケースごとに回す(デフォルトは1)
+ */
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int64_t TESTCASES = 1;
+
+    // std::cin >> TESTCASES;
+
+    for (int64_t i = 0; i < TESTCASES; ++i)
+    {
+        solve();
+    }
+}
+
+//======================
+/**
+ *方針メモ欄
+ *
+ */
+//======================
