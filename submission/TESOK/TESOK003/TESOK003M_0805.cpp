e602baefdc251ee283d4a7acc8d8d582f97e5f0b
Lit-to <litto0315@gmail.com>
2026-08-05 14:08:26 +0000

    AC:TESOK003M.cpp

---
+++ submission/TESOK/TESOK003/TESOK003M_0805.cpp
@@ -0,0 +1,96 @@
+// TESOK003M
+// clang-format off
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+#include <queue>
+#include <atcoder/all>
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define DEFAULT_TESTCASE (1);
+using std::cin;using std::cout;using std::endl;using ll = int64_t;using vll = std::vector<int64_t>;using mint = atcoder::modint998244353;
+// using mint = atcoder::modint1000000007;
+template <typename T>using greater_priority_queue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+template <typename T>T input(){T variable;cin >> variable;return variable;}
+template <typename T>std::vector<T> input(int64_t n){std::vector<T> contents(n);for (int64_t i = 0; i < n; ++i){contents[i] = input<T>();}return contents;}
+// clang-format on
+
+/**
+ * 1ケースぶんの処理実行
+ */
+void solve()
+{
+    /*
+    // const auto S = input<std::string>();
+    // const auto A = input<ll>(10);
+    */
+    const auto N = input<ll>();
+    const auto M = input<ll>();
+    struct EDGE
+    {
+        ll A;
+        ll B;
+    };
+    std::vector<EDGE> ab(M);
+    for (ll i = 0; i < M; ++i)
+    {
+        std::cin >> ab[i].A >> ab[i].B;
+        --ab[i].A;
+        --ab[i].B;
+    }
+    std::vector<std::vector<ll>> edges(N);
+    for (ll i = 0; i < M; ++i)
+    {
+        edges[ab[i].A].push_back(ab[i].B);
+        edges[ab[i].B].push_back(ab[i].A);
+    }
+    std::vector<bool> done(N);
+    auto dfs = [&](auto &self, ll pos) -> ll
+    {
+        done[pos] = true;
+        ll result = 0;
+        for (ll i = 0; i < edges[pos].size(); ++i)
+        {
+            if (!done[edges[pos][i]])
+            {
+                ++result;
+                result += self(self, edges[pos][i]);
+            }
+        }
+        return result;
+    };
+    ll result = dfs(dfs, 0) + 1;
+    if (N == result)
+    {
+        cout << "The graph is connected." << endl;
+    }
+    else
+    {
+        cout << "The graph is not connected." << endl;
+    }
+}
+
+/**
+ * エントリポイント
+ * テストケースごとに回す(デフォルトは1)
+ */
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int64_t TESTCASES = DEFAULT_TESTCASE;
+    // std::cin >> TESTCASES;
+    for (int64_t i = 0; i < TESTCASES; ++i)
+    {
+        solve();
+    }
+}
+
+//======================
+/**
+ *方針メモ欄
+ *
+ */
+//======================
