285172dc9eecfa0e4a547ccf565f95f9e5e97958
Lit-to <litto0315@gmail.com>
2026-08-31 11:52:15 +0000

    Revert "AC:ABC450C.cpp"
    This reverts commit 10eefa552673512ced2e9aaaf82b5379db7bcb1c.

---
+++ sandBox/Print.py
@@ -0,0 +1,27 @@
+class Print:
+    """
+    @brief ファイル指定での読み書きを楽に行うためのクラス
+    お気楽ファイル出力クラス
+    """
+    path:str # ファイルパス
+    encoding:str # 文字コード
+    def __init__(self,path:str,encoding:str="utf8"):
+        """
+        @brief コンストラクタ URLを登録する
+        @param path ファイルパス
+        @param encoding 文字コード(デフォルト:utf8)
+        """
+        self.path = path
+        self.encoding= encoding
+        with open(self.path,encoding=self.encoding,mode="w") as f:
+            print("",file=f,end="")
+            
+    def print(self,*contents:str,sep=" ",end="\n"):
+        """
+        @brief ファイル出力関数
+        @param *contents 出力したい文字列
+        @param sep 複数個出力するときの区切り文字(デフォルト:空文字列)
+        @param emd 出力後につける文字列(デフォルト:改行文字)
+        """
+        with open(self.path,encoding=self.encoding,mode="a") as f:
+            print(*contents,file=f,sep=sep,end=end)
