41c3c40137d2a18ebbaa2386bbe41f5704fe4710
Lit-to <litto0315@gmail.com>
2026-08-31 11:52:43 +0000

    AC:ABC450C.cpp

---
+++ submission/ABC/ABC450/ABC450C_0831.cpp
@@ -0,0 +1,326 @@
+// ABC450C
+// clang-format off
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <atcoder/all>
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define DEFAULT_TESTCASE (1);
+using std::abs;
+using std::cin;using std::cout;using std::endl;using std::vector;using ll = int64_t;using vll = std::vector<int64_t>;using mint = atcoder::modint998244353;
+// using mint = atcoder::modint1000000007;
+template <typename T>T input(){T variable;cin >> variable;return variable;}
+template <typename T>std::vector<T> input(int64_t n){std::vector<T> contents(n);for (int64_t i = 0; i < n; ++i){contents[i] = input<T>();}return contents;}
+// clang-format on
+
+/**
+ * 1ケースぶんの処理実行
+ */
+void solve()
+{
+    /*
+    // const auto N = input<ll>();
+    // const auto S = input<std::string>();
+    // const auto A = input<ll>(10);
+    */
+}
+
+/**
+ * エントリポイント
+ * テストケースごとに回す(デフォルトは1)
+ */
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int64_t TESTCASES = DEFAULT_TESTCASE;
+    // std::cin >> TESTCASES;
+    for (int64_t i = 0; i < TESTCASES; ++i)
+    {
+        solve();
+    }
+}
+
+//======================
+/**
+ *方針メモ欄
+ *
+ */
+//======================
+
+// AtCoder提出用テンプレート
+// 自作ライブラリ・スニペットはここ:https://github.com/Lit-to/atcoder/tree/main/modules/cpp
+// ABC450C
+// clang-format off
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <vector>
+#include <atcoder/all>
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define DEFAULT_TESTCASE (1);
+using std::abs;
+using std::cin;using std::cout;using std::endl;using std::vector;using ll = int64_t;using vll = std::vector<int64_t>;using mint = atcoder::modint998244353;
+// using mint = atcoder::modint1000000007;
+template <typename T>T input(){T variable;cin >> variable;return variable;}
+template <typename T>std::vector<T> input(int64_t n){std::vector<T> contents(n);for (int64_t i = 0; i < n; ++i){contents[i] = input<T>();}return contents;}
+// clang-format on
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+
+/**
+ * 二次元ボードを使いやすくするためのクラス
+ */
+template <class T>
+class Board
+{
+public:
+    /**
+     * @brief 座標情報を持つ構造体
+     */
+    struct POS
+    {
+        int64_t X; //!< 横位置
+        int64_t Y; //!< 縦位置
+    };
+    /**
+     * @brief 座標構造体からインデックス値に変換する関数
+     */
+    int64_t ConvertPosToIndex(const Board::POS &pos) const
+    {
+        return pos.Y * m_width + pos.X;
+    }
+    /**
+     * @brief 座標構造体からインデックス値に変換する関数
+     */
+    int64_t ConvertPosToIndex(const int64_t y, const int64_t x) const
+    {
+        return y * m_width + x;
+    }
+    /**
+     * @brief インデックス値から座標構造体に変換する関数
+     */
+    Board::POS ConvertIndexToPos(const int64_t index) const
+    {
+        return Board::POS{.X = index % m_width, .Y = index / m_width};
+    }
+    /**
+     * コンストラクタ
+     * @brief 縦横でボードを初期化し、各マスをデフォルトコンストラクタ初期化する
+     */
+    Board(int64_t H, int64_t W)
+        : m_height(H), m_width(W), m_size(H * W), m_data(H * W)
+    {
+    }
+    /**
+     * @brief 特定のマスの参照を返す
+     * []演算子オーバーロード
+     */
+    T &operator[](const int64_t y, const int64_t x)
+    {
+        return m_data[ConvertPosToIndex(Board::POS{.X = x, .Y = y})];
+    }
+    /**
+     * @brief 特定のマスのコンスト参照を返す
+     * []演算子オーバーロード
+     */
+    const T &operator[](const int64_t y, const int64_t x) const
+    {
+        return m_data[ConvertPosToIndex(Board::POS{.X = x, .Y = y})];
+    }
+    /**
+     * @brief 特定のマスの参照を返す
+     * []演算子オーバーロード
+     */
+    T &operator[](const int64_t index)
+    {
+        return m_data[index];
+    }
+    /**
+     * @brief 特定のマスのコンスト参照を返す
+     * []演算子オーバーロード
+     */
+    const T &operator[](const int64_t index) const
+    {
+        return m_data[index];
+    }
+    /**
+     * @param index 座標を表すインデックス
+     * @brief 特定のマスがボード範囲内かどうかを返す
+     */
+    bool IsInside(const int64_t index) const
+    {
+        return IsInside(ConvertIndexToPos(index));
+    }
+    /**
+     * @param pos 座標
+     * @brief 特定のマスがボード範囲内かどうかを返す
+     */
+    bool IsInside(const int64_t y, const int64_t x) const
+    {
+        return (0 <= y && y < m_height) && (0 <= x && x < m_width);
+    }
+    /**
+     * @brief インスタンスを値で埋める
+     */
+    void Fill(const T &value)
+    {
+        for (int64_t i = 0; i < m_size; ++i)
+        {
+            m_data[i] = value;
+        }
+    }
+    /**
+     * @brief インスタンスをデフォルトコンストラクタで埋める
+     */
+    void Clear()
+    {
+        Fill(T());
+    }
+    /**
+     * @brief 縦を取得する
+     */
+    int64_t GetHeight() const
+    {
+        return m_height;
+    }
+    /**
+     * @brief 横を取得する
+     */
+    int64_t GetWidth() const
+    {
+        return m_width;
+    }
+    /**
+     * @brief 縦*横の値を取得する
+     */
+    int64_t GetSize() const
+    {
+        return m_size;
+    }
+    std::string toString() const
+    {
+        std::string result = "";
+        for (int64_t i = 0; i < GetHeight(); ++i)
+        {
+            for (int64_t j = 0; j < GetWidth(); ++j)
+            {
+                result += std::to_string(this->operator[](i, j));
+                result += " ";
+            }
+            result += "\n";
+        }
+        return result;
+    }
+    /**
+     * @brief 標準入力からのファクトリ
+     * @details H W H*W回の内容が来る想定
+     */
+    static Board<T> Input()
+    {
+        int64_t height;
+        int64_t width;
+        std::cin >> height;
+        std::cin >> width;
+        Board<T> data = Board::Input(height, width);
+    }
+    /**
+     * @brief 標準入力からのファクトリ
+     * @details H*W回の内容が来る想定
+     * @param H 作る高さ
+     * @param W 作る幅
+     */
+    static Board<T> Input(int64_t H, int64_t W)
+    {
+        Board<T> data = Board(H, W);
+        for (int64_t i = 0; i < data.GetSize(); ++i)
+        {
+            std::cin >> data[i];
+        }
+        return data;
+    }
+
+private:
+    std::vector<T> m_data; //!< マス情報
+    int64_t m_height;      //!< 高さ
+    int64_t m_width;       //!< 幅
+    int64_t m_size;        //!< ボードの全体サイズ
+};
+/**
+ * 1ケースぶんの処理実行
+ */
+void solve()
+{
+    /*
+    // const auto S = input<std::string>();
+    // const auto A = input<ll>(10);
+    */
+    const auto H = input<ll>();
+    const auto W = input<ll>();
+    auto BOARD = Board<char>::Input(H, W);
+    std::unordered_set<ll> fillBlackDone;
+    const int64_t LRUD_4[4][2] = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+
+    /**
+     * DFSスニペット
+     */
+    auto fillBlackDFS = [&](auto self, ll posY, ll posX) -> void // 引数にノード、戻り値は適度に
+    {
+        auto index = BOARD.ConvertPosToIndex(posY, posX);
+        // 訪問済み管理のチェック
+        if (fillBlackDone.contains(index))
+        {
+            return;
+        }
+        if (BOARD[posY, posX] == '#')
+        {
+            return;
+        }
+
+        // 訪問済み管理の登録
+        fillBlackDone.insert(index);
+
+        // 行きがけ順に処理したい内容
+        BOARD[posY, posX] = '#';
+        for (auto &dest : LRUD_4) // 遷移処理
+        {
+            ll destY = posY + dest[0];
+            ll destX = posX + dest[1];
+            self(self, destY, destX);
+        }
+        // 帰りがけ順に処理したい内容
+    };
+}
+
+/**
+ * エントリポイント
+ * テストケースごとに回す(デフォルトは1)
+ */
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int64_t TESTCASES = DEFAULT_TESTCASE;
+    // std::cin >> TESTCASES;
+    for (int64_t i = 0; i < TESTCASES; ++i)
+    {
+        solve();
+    }
+}
+
+//======================
+/**
+ *方針メモ欄
+ *
+ */
+//======================
+
+// AtCoder提出用テンプレート
+// 自作ライブラリ・スニペットはここ:https://github.com/Lit-to/atcoder/tree/main/modules/cpp
