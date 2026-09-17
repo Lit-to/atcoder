b92404866916f2e1d4f030d189ce5b56d22d0dde
Lit-to <litto0315@gmail.com>
2026-09-17 15:12:26 +0000

    modify:イテレータのデータの持ち方をindexからr,cにへんこう

--- modules/cpp/Board.cpp
+++ modules/cpp/Board.cpp
@@ -3,217 +3,200 @@
 #include <iostream>
 
 /**
- * 二次元ボードを使いやすくするためのクラス
+ * 二次元ボード
  */
 template <class T>
 class Board
 {
-public:
     /**
-     * @brief 座標情報を持つ構造体
+     * イテレータ
      */
-    struct POS
+    class Iterator
     {
-        int64_t X; //!< 横位置
-        int64_t Y; //!< 縦位置
-    };
-    /**
-     * @brief 座標構造体からインデックス値に変換する関数
-     */
-    int64_t ConvertPosToIndex(const Board::POS &pos) const
-    {
-        return pos.Y * m_width + pos.X;
-    }
-    /**
-     * @brief 座標構造体からインデックス値に変換する関数
-     */
-    int64_t ConvertPosToIndex(const int64_t y, const int64_t x) const
-    {
-        return y * m_width + x;
-    }
-    /**
-     * @brief インデックス値から座標構造体に変換する関数
-     */
-    Board::POS ConvertIndexToPos(const int64_t index) const
-    {
-        return Board::POS{.X = index % m_width, .Y = index / m_width};
-    }
-    /**
-     * コンストラクタ
-     * @brief 縦横でボードを初期化し、各マスをデフォルトコンストラクタ初期化する
-     */
-    Board(int64_t H, int64_t W)
-        : m_height(H), m_width(W), m_size(H * W), m_data(H * W)
-    {
-    }
 
-    /**
-     * コピーコンストラクタ
-     * @brief 縦横でボードを初期化し、各マスをデフォルトコンストラクタ初期化する
-     */
-    Board(const Board &contents)
-        : m_height(contents.GetHeight()), m_width(contents.GetWidth()), m_size(contents.GetSize()), m_data(contents.GetHeight() * contents.GetWidth())
-    {
-        for (ll i = 0; i < this->GetHeight(); ++i)
+        //==コンストラクタ
+        /**
+         * デフォルトコンストラクタ
+         */
+        Iterator() : m_data(nullptr), m_r(0), m_c(0)
         {
-            for (ll j = 0; j < this->GetWidth(); ++j)
-            {
-                (*this)[i, j] = contents[i, j];
-            }
         }
+
+        /**
+         * ボードと位置から作成するコンストラクタ
+         */
+        Iterator(Board &board, int64_t r, int64_t c) : m_data(&board), m_r(r), m_c(c)
+        {
+        }
+
+        // コピーコンストラクタ
+        Iterator(const Iterator &target) : m_data(target.m_data), m_r(target.m_r), m_c(target.m_c)
+        {
+        }
+
+        //== 演算子,主要メソッド
+        // *演算子(ボードの参照を返す)
+        Iterator &operator*()
+        {
+            return *m_data[m_data->getIndex(m_r, m_c)];
+        }
+
+        void move(int64_t r, int64_t c)
+        {
+            m_r += r;
+            m_c += c;
+        }
+
+        // 左に1つ移動
+        void &moveL()
+        {
+            --m_r;
+        }
+
+        // 右に1つ移動
+        void &moveR()
+        {
+            ++m_r;
+        }
+
+        // 上に1つ移動
+        void &moveU()
+        {
+            --m_c;
+        }
+
+        // 下に1つ移動
+        void &moveD()
+        {
+            ++m_c;
+        }
+
+        // 等価演算子(同じボード、同じ位置かどうか)
+        bool operator=(Iterator &rhs)
+        {
+            return m_data == rhs.m_data && m_r == rhs.m_r && m_c == rhs.m_c;
+        }
+
+        // 不等価演算子
+        bool operator!=(Iterator &rhs)
+        {
+            return (!this == rhs);
+        }
+
+        // ボードの内側かどうか
+        bool isInside() const
+        {
+            return m_data->isInside(m_r, m_c);
+        }
+
+        // ボードの外側かどうか
+        bool isOutside() const
+        {
+            return (!isInside());
+        }
+
+        int64_t getIndex() const
+        {
+            return m_data.getIndex(m_r, m_c);
+        }
+
+        //==メンバー変数
+        Board *m_data; //<!対応するボード
+        int64_t m_r;   //<! 指し示す位置縦方向の位置(index/int64_t)
+        int64_t m_c;   //<! 指し示す位置横方向の位置(index/int64_t)
+    };
+
+    //==コンストラクタ
+    // コンストラクタ
+    Board<T>() : m_data(null), m_height(0), m_width(0)
+    {
     }
 
-    /**
-     * @brief 特定のマスの参照を返す
-     * []演算子オーバーロード
-     */
-    T &operator[](const int64_t y, const int64_t x)
+    // コピーコンストラクタ (ボード) =>(void)
+    Board<T>(Board &rhs) : m_data(rhs.m_data) : m_height(rhs.m_height), m_width(rhs.m_width)
     {
-        return m_data[ConvertPosToIndex(Board::POS{.X = x, .Y = y})];
     }
-    /**
-     * @brief 特定のマスのコンスト参照を返す
-     * []演算子オーバーロード
-     */
-    const T &operator[](const int64_t y, const int64_t x) const
+    // サイズ指定
+    Board<T>(int64_t height, int64_t width) : m_data(height * width), m_height(height), m_width(width)
     {
-        return m_data[ConvertPosToIndex(Board::POS{.X = x, .Y = y})];
-    }
-    /**
-     * @brief 特定のマスの参照を返す
-     * []演算子オーバーロード
-     */
-    T &operator[](const int64_t index)
-    {
-        return m_data[index];
-    }
-    /**
-     * @brief 特定のマスのコンスト参照を返す
-     * []演算子オーバーロード
-     */
-    const T &operator[](const int64_t index) const
-    {
-        return m_data[index];
-    }
-    /**
-     * @param index 座標を表すインデックス
-     * @brief 特定のマスがボード範囲内かどうかを返す
-     */
-    bool IsInside(const int64_t index) const
-    {
-        return IsInside(ConvertIndexToPos(index));
     }
 
-    /**
-     * @param index 座標を表すインデックス
-     * @brief 特定のマスがボード範囲外かどうかを返す
-     */
-    bool IsOutside(const int64_t index) const
+    // サイズ指定・デフォルト埋めコンストラクタ (サイズ,値)=>(void)
+    Board<T>(int64_t height, int64_t width, T value) : m_data(height * width, value), m_height(height), m_width(width)
     {
-        return !IsInside(ConvertIndexToPos(index));
     }
 
-    /**
-     * @param pos 座標
-     * @brief 特定のマスがボード範囲内かどうかを返す
-     */
-    bool IsInside(const int64_t y, const int64_t x) const
+    //==主要メソッド
+    // 座標からインデックスの取得
+    int64_t getIndex(int64_t r, int64_t c)
     {
-        return (0 <= y && y < m_height) && (0 <= x && x < m_width);
+        return r * m_width + c;
     }
 
-    /**
-     * @param pos 座標
-     * @brief 特定のマスがボード範囲外かどうかを返す
-     */
-    bool IsOutside(const int64_t y, const int64_t x) const
+    // 特定位置のイテレータ取得(r,c)=>itr
+    Iterator getIterator(int64_t r, int64_t c)
     {
-        return !IsInside(y, x);
+        return Iterator(this, r, c);
     }
 
-    /**
-     * @brief インスタンスを値で埋める
-     */
-    void Fill(const T &value)
+    // 先頭(左上位置)のイテレータ取得?()=>itr
+    Iterator getFirst()
     {
-        for (int64_t i = 0; i < m_size; ++i)
+        return getIterator(0, 0);
+    }
+
+    // 全部埋め(値)=>void
+    void fill(T value)
+    {
+        for (int64_t i = 0; i < getSize(); ++i)
         {
             m_data[i] = value;
         }
     }
-    /**
-     * @brief インスタンスをデフォルトコンストラクタで埋める
-     */
-    void Clear()
+
+    // ある位置がボードの内側かどうか(r,c)=>bool
+    bool isInside(int64_t r, int64_t c)
     {
-        Fill(T());
-    }
-    /**
-     * @brief 縦を取得する
-     */
-    int64_t GetHeight() const
-    {
-        return m_height;
-    }
-    /**
-     * @brief 横を取得する
-     */
-    int64_t GetWidth() const
-    {
-        return m_width;
-    }
-    /**
-     * @brief 縦*横の値を取得する
-     */
-    int64_t GetSize() const
-    {
-        return m_size;
-    }
-    std::string toString() const
-    {
-        std::string result = "";
-        for (int64_t i = 0; i < GetHeight(); ++i)
-        {
-            for (int64_t j = 0; j < GetWidth(); ++j)
-            {
-                result += std::to_string(this->operator[](i, j));
-                result += " ";
-            }
-            result += "\n";
-        }
-        return result;
-    }
-    /**
-     * @brief 標準入力からのファクトリ
-     * @details H W H*W回の内容が来る想定
-     */
-    static Board<T> Input()
-    {
-        int64_t height;
-        int64_t width;
-        std::cin >> height;
-        std::cin >> width;
-        Board<T> data = Board::Input(height, width);
-    }
-    /**
-     * @brief 標準入力からのファクトリ
-     * @details H*W回の内容が来る想定
-     * @param H 作る高さ
-     * @param W 作る幅
-     */
-    static Board<T> Input(int64_t H, int64_t W)
-    {
-        Board<T> data = Board(H, W);
-        for (int64_t i = 0; i < data.GetSize(); ++i)
-        {
-            std::cin >> data[i];
-        }
-        return data;
+        int64_t pos = r * m_width + c;
+        return 0 <= pos && pos < getSize();
     }
 
-private:
-    std::vector<T> m_data; //!< マス情報
-    int64_t m_height;      //!< 高さ
-    int64_t m_width;       //!< 幅
-    int64_t m_size;        //!< ボードの全体サイズ
+    // ある位置がボードの外側かどうか(r,c)=>bool
+    bool isOutside(int64_t r, int64_t c)
+    {
+        return !isInside();
+    }
+
+    // 特定位置の値取得(r,c)
+
+    T &getRef(int64_t r, int64_t c)
+    {
+        return m_data[getIndex(r, c)];
+    }
+
+    // 指定位置の参照取得(r,c)
+    T &getValue(int64_t r, int64_t c)
+    {
+        return *getRef(r, c);
+    }
+
+    // サイズ取得
+    int64_t getSize()
+    {
+        return m_height * m_width;
+    }
+
+    // 位置取得
+    int64_t getIndex(int64_t r, int64_t c)
+    {
+        return r * m_width + c;
+    }
+
+    //==メンバ変数
+    vector<T> m_data; //<! データ実体
+    int64_t m_height; //<! 高さ
+    int64_t m_width;  //<! 横幅
+
+    // フレンド登録
+    friend Iterator;
 };
