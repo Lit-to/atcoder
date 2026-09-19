27270bdb12d97f101d1f47087703065f3dd557e9
Lit-to <litto0315@gmail.com>
2026-09-19 05:53:30 +0000

    AC:ABC450C.cpp

--- submission/ABC/ABC450/ABC450C_0919.cpp
+++ submission/ABC/ABC450/ABC450C_0919.cpp
@@ -1,3 +1,56 @@
+// ABC450C
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <atcoder/all>
+
+#include <map>
+template <class KEY_TYPE, class VALUE_TYPE>
+using TreeMap = std::map<KEY_TYPE, VALUE_TYPE>;
+#include <unordered_map>
+template <class KEY_TYPE, class VALUE_TYPE>
+using HashMap = std::unordered_map<KEY_TYPE, VALUE_TYPE>;
+
+#include <set>
+template <class VALUE_TYPE>
+using TreeSet = std::set<VALUE_TYPE>;
+#include <unordered_set>
+template <class VALUE_TYPE>
+using HashSet = std::unordered_set<VALUE_TYPE>;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define DEFAULT_TESTCASE (1);
+using std::abs;
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using ll = int64_t;
+using vll = std::vector<int64_t>;
+using mint = atcoder::modint998244353;
+// using mint = atcoder::modint1000000007;
+template <typename T>
+T input()
+{
+    T variable;
+    cin >> variable;
+    return variable;
+}
+template <typename T>
+std::vector<T> input(int64_t n)
+{
+    std::vector<T> contents(n);
+    for (int64_t i = 0; i < n; ++i)
+    {
+        contents[i] = input<T>();
+    }
+    return contents;
+}
+// 自作ライブラリここから
 #include <stdexcept>
 #include <vector>
 #include <iostream>
@@ -231,3 +284,104 @@ public:
     // フレンド登録
     friend Iterator;
 };
+
+// 自作ライブラリここまで
+/**
+ * 1ケースぶんの処理実行
+ */
+void solve()
+{
+    // 入力スニペ
+    // const auto S = input<std::string>();
+    // const auto A = input<ll>(N);
+    //
+    const auto H = input<ll>();
+    const auto W = input<ll>();
+    Board<char> BOARD(H, W);
+    cin >> BOARD;
+    const int64_t LRUD_4[4][2] = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+    /**
+     * DFSスニペット
+     */
+    auto dfs = [&](auto self, Board<char>::Iterator pos) -> void // 引数にノード、戻り値は適度に
+    {
+        // 訪問済み管理のチェック
+        if (BOARD.isOutside(pos.m_r, pos.m_c))
+        {
+            return;
+        }
+        if (*pos != '.')
+        {
+            return;
+        }
+        // 訪問済み管理の登録
+        (*pos) = '#';
+        // 行きがけ順に処理したい内容
+        for (auto &dest : LRUD_4) // 遷移処理
+        {
+            self(self, pos.getMoved(dest[0], dest[1]));
+        }
+        // 帰りがけ順に処理したい内容
+    };
+    for (ll i = 0; i < H; ++i)
+    {
+        if (BOARD.getValue(i, 0) == '.')
+        {
+            dfs(dfs, BOARD.getIterator(i, 0));
+        }
+        if (BOARD.getValue(i, W - 1) == '.')
+        {
+            dfs(dfs, BOARD.getIterator(i, W - 1));
+        }
+    }
+    for (ll i = 0; i < W; ++i)
+    {
+        if (BOARD.getValue(0, i) == '.')
+        {
+            dfs(dfs, BOARD.getIterator(0, i));
+        }
+        if (BOARD.getValue(H - 1, i) == '.')
+        {
+            dfs(dfs, BOARD.getIterator(H - 1, i));
+        }
+    }
+    ll result = 0;
+    for (ll i = 0; i < H; ++i)
+    {
+        for (ll j = 0; j < W; ++j)
+        {
+            if (BOARD.getValue(i, j) == '.')
+            {
+                ++result;
+                dfs(dfs, BOARD.getIterator(i, j));
+            }
+        }
+    }
+    cout << result << endl;
+}
+
+/**
+ * エントリポイント
+ * テストケースごとに回す(デフォルトは1)
+ */
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int64_t TESTCASES = DEFAULT_TESTCASE;
+    // std::cin >> TESTCASES;
+    for (int64_t i = 0; i < TESTCASES; ++i)
+    {
+        solve();
+    }
+}
+
+//======================
+/**
+ *方針メモ欄
+ *
+ */
+//======================
+
+// AtCoder提出用テンプレート
+// 自作ライブラリ・スニペットはここ:https://github.com/Lit-to/atcoder/tree/main/modules/cpp
