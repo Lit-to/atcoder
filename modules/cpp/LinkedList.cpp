294e458beaa9da4eca6172b279d9fc3c088eaf9c
Lit-to <litto0315@gmail.com>
2026-09-20 06:24:38 +0000

    fix:const参照が返ってなかったのを修正

--- modules/cpp/LinkedList.cpp
+++ modules/cpp/LinkedList.cpp
@@ -268,7 +268,7 @@ public:
     /**
      * 先頭の参照を取得(const版)
      */
-    T &GetFront() const
+    const T &GetFront() const
     {
         return *GetBegin();
     }
@@ -284,7 +284,7 @@ public:
     /**
      * 末尾の参照を取得(const版)
      */
-    T &GetBack() const
+    const T &GetBack() const
     {
         return *(--GetEnd());
     }
@@ -301,7 +301,7 @@ public:
     /**
      * 先頭イテレータの取得(const版)
      */
-    Iterator GetBegin() const
+    const Iterator GetBegin() const
     {
         auto retVal = m_sentinel;
         return ++retVal;
@@ -318,7 +318,7 @@ public:
     /**
      * 末尾イテレータの取得(const版)
      */
-    Iterator GetEnd() const
+    const Iterator GetEnd() const
     {
         return m_sentinel;
     }
